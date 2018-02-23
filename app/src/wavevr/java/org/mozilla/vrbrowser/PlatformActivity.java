1a2a1d9f7184982572bd711f6b7ec54fcf0116f3
Imanol Fernandez <mortimergoro@gmail.com>
2018-02-23 16:27:50 +0100

    Fix for Android widgets in Vive Focus

--- app/src/wavevr/java/org/mozilla/vrbrowser/PlatformActivity.java
+++ app/src/wavevr/java/org/mozilla/vrbrowser/PlatformActivity.java
@@ -8,9 +8,17 @@ package org.mozilla.vrbrowser;
 import com.htc.vr.sdk.VRActivity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
+import java.lang.reflect.Field;
 
 public class PlatformActivity extends VRActivity {
+    static final String LOGTAG = "VRB";
+    private FrameLayout mFrameLayout;
+
     public PlatformActivity() {
         super.setUsingRenderBaseActivity(true);
     }
@@ -18,6 +26,20 @@ public class PlatformActivity extends VRActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Render to our SurfaceView instead of letting VRActivity take ownership of the window.
+        SurfaceHolder.Callback2 callback = getVRActivitySurfaceCallback();
+        if (callback != null) {
+            getWindow().takeSurface(null);
+            SurfaceView surfaceView = new SurfaceView(this);
+            surfaceView.setClickable(true);
+            surfaceView.getHolder().addCallback(callback);
+            surfaceView.setZOrderOnTop(true);
+            mFrameLayout = new FrameLayout(this);
+            mFrameLayout.addView(surfaceView, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+            setContentView(mFrameLayout);
+        }
+
         queueRunnable(new Runnable() {
             @Override
             public void run() {
@@ -27,8 +49,27 @@ public class PlatformActivity extends VRActivity {
     }
 
     protected void addWidget(View aView, int aWidth, int aHeight) {
+        if (mFrameLayout != null) {
+            mFrameLayout.addView(aView, 0, new FrameLayout.LayoutParams(aWidth, aHeight));
+        }
     }
 
+    private SurfaceHolder.Callback2 getVRActivitySurfaceCallback() {
+        try {
+            Field fPlatform = VRActivity.class.getDeclaredField("mVRPlatform");
+            fPlatform.setAccessible(true);
+            Object platform = fPlatform.get(this);
+            Field fRenderer = platform.getClass().getDeclaredField("mSVRRenderBase");
+            fRenderer.setAccessible(true);
+            Object renderer = fRenderer.get(platform);
+            if (renderer != null && renderer instanceof SurfaceHolder.Callback) {
+                return (SurfaceHolder.Callback2) renderer;
+            }
+        } catch (Exception e) {
+            Log.e(LOGTAG, "Error getting SurfaceHolder.Callback from VRActivity:" + e.toString());
+        }
+        return  null;
+    }
 
     protected native void queueRunnable(Runnable aRunnable);
     protected native void initializeJava(AssetManager aAssets);
