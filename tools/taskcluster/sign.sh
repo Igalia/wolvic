4fd77ace56fa38058caf581bb7e1e3159663350e
Randall Barker <rbarker@mozilla.com>
2018-08-01 12:18:21 -0700

    Update taskcluster builds to archive debug builds also

--- tools/taskcluster/sign.sh
+++ tools/taskcluster/sign.sh
@@ -3,3 +3,4 @@ python tools/taskcluster/fetch_secret.py -s project/firefoxreality/preview-keyst
 python tools/taskcluster/fetch_secret.py -s project/firefoxreality/keystore-password -o keystore_password -n password
 python tools/taskcluster/fetch_secret.py -s project/firefoxreality/key-password -o key_password -n password
 python tools/taskcluster/sign_apk.py -s keystore.jks -p keystore_password -k key_password -a FirefoxRealityTestKey
+python tools/taskcluster/archive_debug_apk.py
