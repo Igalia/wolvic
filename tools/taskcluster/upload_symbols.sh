e93d1204e9c43c7af25a3f91812a421739d408e3
Randall Barker <rbarker@mozilla.com>
2018-10-01 13:26:32 -0700

    fix symbol upload

--- tools/taskcluster/upload_symbols.sh
+++ tools/taskcluster/upload_symbols.sh
@@ -4,8 +4,8 @@ git clone git@github.com:MozillaReality/symbolgenerator.git tools/taskcluster/sy
 export PYTHONPATH="tools/taskcluster/symbols/third_party/python/redo:tools/taskcluster/symbols/third_party/python/requests"
 export OBJCOPY="tools/taskcluster/symbols/bin/arm-linux-androideabi-objcopy"
 
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/googlevr/release/obj/armeabi-v7a/libnative-lib.so googlevr
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/noapi/release/obj/armeabi-v7a/libnative-lib.so noapi
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/wavevr/release/obj/armeabi-v7a/libnative-lib.so wavevr
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/svr/release/obj/armeabi-v7a/libnative-lib.so svr
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/oculusvr/release/obj/armeabi-v7a/libnative-lib.so oculusvr
+python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/googlevrArm/release/obj/armeabi-v7a/libnative-lib.so googlevr
+python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/noapiArm/release/obj/armeabi-v7a/libnative-lib.so noapi
+python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/wavevrArm/release/obj/armeabi-v7a/libnative-lib.so wavevr
+python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/svrArm/release/obj/armeabi-v7a/libnative-lib.so svr
+python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/oculusvrArm/release/obj/armeabi-v7a/libnative-lib.so oculusvr
