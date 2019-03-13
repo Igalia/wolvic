ncalexan/FirefoxReality

Update Settings Dialog to allow for better scaling

app/src/common/shared/org/mozilla/vrbrowser/browser/SessionStore.java
@@ -89,7 +89,7 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
     class SessionSettings {
         boolean multiprocess = SettingsStore.getInstance(mContext).isMultiprocessEnabled();
         boolean privateMode = false;
-        boolean trackingProtection = true;
+        boolean trackingProtection = SettingsStore.getInstance(mContext).isTrackingProtectionEnabled();
         boolean suspendMediaWhenInactive = true;
         int userAgentMode = SettingsStore.getInstance(mContext).getUaMode();
         boolean servo = false;
@@ -872,7 +872,7 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
       }
     }
 
-    public void setMultiprocess(final boolean enabled) {
+    private void recreateSession(SessionStore.SessionSettings aSettings) {
         if (mCurrentSession != null) {
             final GeckoResult<GeckoSession.SessionState> state = mCurrentSession.saveState();
             state.then(new GeckoResult.OnValueListener<GeckoSession.SessionState, Object>() {
@@ -884,9 +884,7 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
                         mCurrentSession.close();
 
                         int oldSessionId = getCurrentSessionId();
-                        SessionStore.SessionSettings settings = new SessionStore.SessionSettings();
-                        settings.multiprocess = enabled;
-                        int sessionId = createSession(settings);
+                        int sessionId = createSession(aSettings);
                         GeckoSession session = getSession(sessionId);
                         session.restoreState(value);
                         setCurrentSession(sessionId);
@@ -903,6 +901,30 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
                     return null;
                 }
             });
+        }
+    }
+
+
+    private State getCurrentState() {
+        if (mCurrentSession != null) {
+            return mSessions.get(mCurrentSession.hashCode());
+        }
+        return null;
+    }
+
+    public void setMultiprocess(final boolean aEnabled) {
+        State state = getCurrentState();
+        if (state != null && state.mSettings.multiprocess != aEnabled) {
+            state.mSettings.multiprocess = aEnabled;
+            recreateSession(state.mSettings);
+        }
+    }
+
+    public void setTrackingEnabled(final boolean aEnabled) {
+        State state = getCurrentState();
+        if (state != null && state.mSettings.trackingProtection != aEnabled) {
+            state.mSettings.trackingProtection = aEnabled;
+            recreateSession(state.mSettings);
         }
     }
 

app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/dialogs/EnvironmentOptionsWidget.java
@@ -1,0 +1,194 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.vrbrowser.ui.widgets.dialogs;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ScrollView;
+
+import org.mozilla.vrbrowser.R;
+import org.mozilla.vrbrowser.audio.AudioEngine;
+import org.mozilla.vrbrowser.browser.SettingsStore;
+import org.mozilla.vrbrowser.ui.views.UIButton;
+import org.mozilla.vrbrowser.ui.views.settings.ButtonSetting;
+import org.mozilla.vrbrowser.ui.views.settings.RadioGroupSetting;
+import org.mozilla.vrbrowser.ui.views.settings.SwitchSetting;
+import org.mozilla.vrbrowser.ui.widgets.UIWidget;
+import org.mozilla.vrbrowser.ui.widgets.WidgetManagerDelegate;
+import org.mozilla.vrbrowser.ui.widgets.WidgetPlacement;
+import org.mozilla.vrbrowser.utils.LocaleUtils;
+
+public class EnvironmentOptionsWidget extends UIWidget implements
+        WidgetManagerDelegate.WorldClickListener,
+        WidgetManagerDelegate.FocusChangeListener {
+
+    private AudioEngine mAudio;
+    private UIButton mBackButton;
+
+    private SwitchSetting mEnvOverrideSwitch;
+    private RadioGroupSetting mEnvironmentsRadio;
+    private int mRestartDialogHandle = -1;
+
+    private ButtonSetting mResetButton;
+
+    private ScrollView mScrollbar;
+
+    public EnvironmentOptionsWidget(Context aContext) {
+        super(aContext);
+        initialize(aContext);
+    }
+
+    public EnvironmentOptionsWidget(Context aContext, AttributeSet aAttrs) {
+        super(aContext, aAttrs);
+        initialize(aContext);
+    }
+
+    public EnvironmentOptionsWidget(Context aContext, AttributeSet aAttrs, int aDefStyle) {
+        super(aContext, aAttrs, aDefStyle);
+        initialize(aContext);
+    }
+
+    private void initialize(Context aContext) {
+        inflate(aContext, R.layout.options_environment, this);
+
+        mAudio = AudioEngine.fromContext(aContext);
+
+        mWidgetManager.addFocusChangeListener(this);
+        mWidgetManager.addWorldClickListener(this);
+
+        mBackButton = findViewById(R.id.backButton);
+        mBackButton.setOnClickListener(view -> {
+            if (mAudio != null) {
+                mAudio.playSound(AudioEngine.Sound.CLICK);
+            }
+
+            onDismiss();
+        });
+
+        String env = SettingsStore.getInstance(getContext()).getEnvironment();
+        mEnvironmentsRadio = findViewById(R.id.environmentRadio);
+        mEnvironmentsRadio.setOnCheckedChangeListener(mEnvsListener);
+        setEnv(mEnvironmentsRadio.getIdForValue(env), false);
+
+        mEnvOverrideSwitch = findViewById(R.id.envOverrideSwitch);
+        mEnvOverrideSwitch.setOnCheckedChangeListener(mEnvOverrideListener);
+        setEnvOverride(SettingsStore.getInstance(getContext()).isEnvironmentOverrideEnabled());
+
+        mResetButton = findViewById(R.id.resetButton);
+        mResetButton.setOnClickListener(mResetListener);
+
+        mScrollbar = findViewById(R.id.scrollbar);
+    }
+
+    @Override
+    protected void initializeWidgetPlacement(WidgetPlacement aPlacement) {
+        aPlacement.visible = false;
+        aPlacement.width =  WidgetPlacement.dpDimension(getContext(), R.dimen.developer_options_width);
+        aPlacement.height = WidgetPlacement.dpDimension(getContext(), R.dimen.developer_options_height);
+        aPlacement.parentAnchorX = 0.5f;
+        aPlacement.parentAnchorY = 0.5f;
+        aPlacement.anchorX = 0.5f;
+        aPlacement.anchorY = 0.5f;
+        aPlacement.translationY = WidgetPlacement.unitFromMeters(getContext(), R.dimen.restart_dialog_world_y);
+        aPlacement.translationZ = WidgetPlacement.unitFromMeters(getContext(), R.dimen.restart_dialog_world_z);
+    }
+
+    @Override
+    public void releaseWidget() {
+        mWidgetManager.removeFocusChangeListener(this);
+        mWidgetManager.removeWorldClickListener(this);
+
+        super.releaseWidget();
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        mScrollbar.scrollTo(0, 0);
+    }
+
+    private void setEnvOverride(boolean value) {
+        mEnvOverrideSwitch.setOnCheckedChangeListener(null);
+        mEnvOverrideSwitch.setValue(value, false);
+        mEnvOverrideSwitch.setOnCheckedChangeListener(mEnvOverrideListener);
+
+        SettingsStore.getInstance(getContext()).setEnvironmentOverrideEnabled(value);
+    }
+
+    private OnClickListener mResetListener = (view) -> {
+        boolean restart = false;
+        if (mEnvOverrideSwitch.isChecked() != SettingsStore.ENV_OVERRIDE_DEFAULT) {
+            setEnvOverride(SettingsStore.ENV_OVERRIDE_DEFAULT);
+            restart = true;
+        }
+
+        if (!mEnvironmentsRadio.getValueForId(mEnvironmentsRadio.getCheckedRadioButtonId()).equals(SettingsStore.ENV_DEFAULT)) {
+            setEnv(mEnvironmentsRadio.getIdForValue(SettingsStore.ENV_DEFAULT), true);
+        }
+
+        if (restart)
+            showRestartDialog();
+    };
+
+    private SwitchSetting.OnCheckedChangeListener mEnvOverrideListener = (compoundButton, value, doApply) -> {
+        setEnvOverride(value);
+        showRestartDialog();
+    };
+
+    private RadioGroupSetting.OnCheckedChangeListener mEnvsListener = (radioGroup, checkedId, doApply) -> {
+        setEnv(checkedId, doApply);
+    };
+
+    private void setEnv(int checkedId, boolean doApply) {
+        mEnvironmentsRadio.setOnCheckedChangeListener(null);
+        mEnvironmentsRadio.setChecked(checkedId, doApply);
+        mEnvironmentsRadio.setOnCheckedChangeListener(mEnvsListener);
+
+        SettingsStore.getInstance(getContext()).setEnvironment((String) mEnvironmentsRadio.getValueForId(checkedId));
+
+        if (doApply) {
+            mWidgetManager.updateEnvironment();
+        }
+    }
+
+    private void showRestartDialog() {
+        hide(UIWidget.REMOVE_WIDGET);
+
+        UIWidget widget = getChild(mRestartDialogHandle);
+        if (widget == null) {
+            widget = createChild(RestartDialogWidget.class, false);
+            mRestartDialogHandle = widget.getHandle();
+            widget.setDelegate(() -> onRestartDialogDismissed());
+        }
+
+        widget.show();
+    }
+
+    private void onRestartDialogDismissed() {
+        show();
+    }
+
+    // WindowManagerDelegate.FocusChangeListener
+
+    @Override
+    public void onGlobalFocusChanged(View oldFocus, View newFocus) {
+        if (oldFocus == this && isVisible() && findViewById(newFocus.getId()) == null) {
+            onDismiss();
+        }
+    }
+
+    // WorldClickListener
+
+    @Override
+    public void onWorldClick() {
+        if (isVisible()) {
+            onDismiss();
+        }
+    }
+
+}

Igalia/wolvic

app/src/common/shared/org/mozilla/vrbrowser/browser/SessionStore.java
@@ -89,7 +89,7 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
     class SessionSettings {
         boolean multiprocess = SettingsStore.getInstance(mContext).isMultiprocessEnabled();
         boolean privateMode = false;
-        boolean trackingProtection = true;
+        boolean trackingProtection = SettingsStore.getInstance(mContext).isTrackingProtectionEnabled();
         boolean suspendMediaWhenInactive = true;
         int userAgentMode = SettingsStore.getInstance(mContext).getUaMode();
         boolean servo = false;
@@ -872,7 +872,7 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
       }
     }
 
-    public void setMultiprocess(final boolean enabled) {
+    private void recreateSession(SessionStore.SessionSettings aSettings) {
         if (mCurrentSession != null) {
             final GeckoResult<GeckoSession.SessionState> state = mCurrentSession.saveState();
             state.then(new GeckoResult.OnValueListener<GeckoSession.SessionState, Object>() {
@@ -884,9 +884,7 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
                         mCurrentSession.close();
 
                         int oldSessionId = getCurrentSessionId();
-                        SessionStore.SessionSettings settings = new SessionStore.SessionSettings();
-                        settings.multiprocess = enabled;
-                        int sessionId = createSession(settings);
+                        int sessionId = createSession(aSettings);
                         GeckoSession session = getSession(sessionId);
                         session.restoreState(value);
                         setCurrentSession(sessionId);
@@ -903,6 +901,29 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
                     return null;
                 }
             });
+        }
+    }
+
+    private State getCurrentState() {
+        if (mCurrentSession != null) {
+            return mSessions.get(mCurrentSession.hashCode());
+        }
+        return null;
+    }
+
+    public void setMultiprocess(final boolean aEnabled) {
+        State state = getCurrentState();
+        if (state != null && state.mSettings.multiprocess != aEnabled) {
+            state.mSettings.multiprocess = aEnabled;
+            recreateSession(state.mSettings);
+        }
+    }
+
+    public void setTrackingProtection(final boolean aEnabled) {
+        State state = getCurrentState();
+        if (state != null && state.mSettings.trackingProtection != aEnabled) {
+            state.mSettings.trackingProtection = aEnabled;
+            recreateSession(state.mSettings);
         }
     }
 

app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/dialogs/EnvironmentOptionsWidget.java
@@ -1,0 +1,201 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.vrbrowser.ui.widgets.dialogs;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ScrollView;
+
+import org.mozilla.vrbrowser.R;
+import org.mozilla.vrbrowser.audio.AudioEngine;
+import org.mozilla.vrbrowser.browser.SessionStore;
+import org.mozilla.vrbrowser.browser.SettingsStore;
+import org.mozilla.vrbrowser.ui.views.UIButton;
+import org.mozilla.vrbrowser.ui.views.settings.ButtonSetting;
+import org.mozilla.vrbrowser.ui.views.settings.ImageRadioGroupSetting;
+import org.mozilla.vrbrowser.ui.views.settings.RadioGroupSetting;
+import org.mozilla.vrbrowser.ui.views.settings.SwitchSetting;
+import org.mozilla.vrbrowser.ui.widgets.UIWidget;
+import org.mozilla.vrbrowser.ui.widgets.WidgetManagerDelegate;
+import org.mozilla.vrbrowser.ui.widgets.WidgetPlacement;
+import org.mozilla.vrbrowser.utils.LocaleUtils;
+
+public class EnvironmentOptionsWidget extends UIWidget implements
+        WidgetManagerDelegate.WorldClickListener,
+        WidgetManagerDelegate.FocusChangeListener {
+
+    private AudioEngine mAudio;
+    private UIButton mBackButton;
+
+    private SwitchSetting mEnvOverrideSwitch;
+    private ImageRadioGroupSetting mEnvironmentsRadio;
+    private int mRestartDialogHandle = -1;
+
+    private ButtonSetting mResetButton;
+
+    private ScrollView mScrollbar;
+
+    public EnvironmentOptionsWidget(Context aContext) {
+        super(aContext);
+        initialize(aContext);
+    }
+
+    public EnvironmentOptionsWidget(Context aContext, AttributeSet aAttrs) {
+        super(aContext, aAttrs);
+        initialize(aContext);
+    }
+
+    public EnvironmentOptionsWidget(Context aContext, AttributeSet aAttrs, int aDefStyle) {
+        super(aContext, aAttrs, aDefStyle);
+        initialize(aContext);
+    }
+
+    private void initialize(Context aContext) {
+        inflate(aContext, R.layout.options_environment, this);
+
+        mAudio = AudioEngine.fromContext(aContext);
+
+        mWidgetManager.addFocusChangeListener(this);
+        mWidgetManager.addWorldClickListener(this);
+
+        mBackButton = findViewById(R.id.backButton);
+        mBackButton.setOnClickListener(view -> {
+            if (mAudio != null) {
+                mAudio.playSound(AudioEngine.Sound.CLICK);
+            }
+
+            onDismiss();
+        });
+
+        String env = SettingsStore.getInstance(getContext()).getEnvironment();
+        mEnvironmentsRadio = findViewById(R.id.environmentRadio);
+        mEnvironmentsRadio.setOnCheckedChangeListener(mEnvsListener);
+        setEnv(mEnvironmentsRadio.getIdForValue(env), false);
+
+        mEnvOverrideSwitch = findViewById(R.id.envOverrideSwitch);
+        mEnvOverrideSwitch.setOnCheckedChangeListener(mEnvOverrideListener);
+        setEnvOverride(SettingsStore.getInstance(getContext()).isEnvironmentOverrideEnabled());
+        mEnvOverrideSwitch.setHelpDelegate(() -> {
+            if (mAudio != null) {
+                mAudio.playSound(AudioEngine.Sound.CLICK);
+            }
+            SessionStore.get().loadUri(getContext().getString(R.string.environment_override_help_url));
+            hide(REMOVE_WIDGET);
+        });
+
+        mResetButton = findViewById(R.id.resetButton);
+        mResetButton.setOnClickListener(mResetListener);
+
+        mScrollbar = findViewById(R.id.scrollbar);
+    }
+
+    @Override
+    protected void initializeWidgetPlacement(WidgetPlacement aPlacement) {
+        aPlacement.visible = false;
+        aPlacement.width =  WidgetPlacement.dpDimension(getContext(), R.dimen.developer_options_width);
+        aPlacement.height = WidgetPlacement.dpDimension(getContext(), R.dimen.developer_options_height);
+        aPlacement.parentAnchorX = 0.5f;
+        aPlacement.parentAnchorY = 0.5f;
+        aPlacement.anchorX = 0.5f;
+        aPlacement.anchorY = 0.5f;
+        aPlacement.translationY = WidgetPlacement.unitFromMeters(getContext(), R.dimen.restart_dialog_world_y);
+        aPlacement.translationZ = WidgetPlacement.unitFromMeters(getContext(), R.dimen.restart_dialog_world_z);
+    }
+
+    @Override
+    public void releaseWidget() {
+        mWidgetManager.removeFocusChangeListener(this);
+        mWidgetManager.removeWorldClickListener(this);
+
+        super.releaseWidget();
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        mScrollbar.scrollTo(0, 0);
+    }
+
+    private void setEnvOverride(boolean value) {
+        mEnvOverrideSwitch.setOnCheckedChangeListener(null);
+        mEnvOverrideSwitch.setValue(value, false);
+        mEnvOverrideSwitch.setOnCheckedChangeListener(mEnvOverrideListener);
+
+        SettingsStore.getInstance(getContext()).setEnvironmentOverrideEnabled(value);
+    }
+
+    private OnClickListener mResetListener = (view) -> {
+        boolean restart = false;
+        if (mEnvOverrideSwitch.isChecked() != SettingsStore.ENV_OVERRIDE_DEFAULT) {
+            setEnvOverride(SettingsStore.ENV_OVERRIDE_DEFAULT);
+            restart = true;
+        }
+
+        if (!mEnvironmentsRadio.getValueForId(mEnvironmentsRadio.getCheckedRadioButtonId()).equals(SettingsStore.ENV_DEFAULT)) {
+            setEnv(mEnvironmentsRadio.getIdForValue(SettingsStore.ENV_DEFAULT), true);
+        }
+
+        if (restart)
+            showRestartDialog();
+    };
+
+    private SwitchSetting.OnCheckedChangeListener mEnvOverrideListener = (compoundButton, value, doApply) -> {
+        setEnvOverride(value);
+        showRestartDialog();
+    };
+
+    private ImageRadioGroupSetting.OnCheckedChangeListener mEnvsListener = (checkedId, doApply) -> {
+        setEnv(checkedId, doApply);
+    };
+
+    private void setEnv(int checkedId, boolean doApply) {
+        mEnvironmentsRadio.setOnCheckedChangeListener(null);
+        mEnvironmentsRadio.setChecked(checkedId, doApply);
+        mEnvironmentsRadio.setOnCheckedChangeListener(mEnvsListener);
+
+        SettingsStore.getInstance(getContext()).setEnvironment((String) mEnvironmentsRadio.getValueForId(checkedId));
+
+        if (doApply) {
+            mWidgetManager.updateEnvironment();
+        }
+    }
+
+    private void showRestartDialog() {
+        hide(UIWidget.REMOVE_WIDGET);
+
+        UIWidget widget = getChild(mRestartDialogHandle);
+        if (widget == null) {
+            widget = createChild(RestartDialogWidget.class, false);
+            mRestartDialogHandle = widget.getHandle();
+            widget.setDelegate(() -> onRestartDialogDismissed());
+        }
+
+        widget.show();
+    }
+
+    private void onRestartDialogDismissed() {
+        show();
+    }
+
+    // WindowManagerDelegate.FocusChangeListener
+    @Override
+    public void onGlobalFocusChanged(View oldFocus, View newFocus) {
+        if (oldFocus == this && isVisible() && findViewById(newFocus.getId()) == null) {
+            onDismiss();
+        }
+    }
+
+    // WorldClickListener
+    @Override
+    public void onWorldClick() {
+        if (isVisible()) {
+            onDismiss();
+        }
+    }
+
+}