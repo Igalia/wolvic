b2632d53411be4cba33e148795631b422536d1d4
Imanol Fernandez <mortimergoro@gmail.com>
2019-05-08 20:17:45 +0200

    Implement french keyboard (#1174)

---
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/keyboards/FrenchKeyboard.java
@@ -0,0 +1,45 @@
+package org.mozilla.vrbrowser.ui.keyboards;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.mozilla.vrbrowser.R;
+import org.mozilla.vrbrowser.input.CustomKeyboard;
+import org.mozilla.vrbrowser.utils.StringUtils;
+
+import java.util.Locale;
+
+public class FrenchKeyboard extends BaseKeyboard {
+    private CustomKeyboard mKeyboard;
+
+    public FrenchKeyboard(Context aContext) {
+        super(aContext);
+    }
+
+    @NonNull
+    @Override
+    public CustomKeyboard getAlphabeticKeyboard() {
+        if (mKeyboard == null) {
+            mKeyboard = new CustomKeyboard(mContext.getApplicationContext(), R.xml.keyboard_qwerty_french);
+        }
+        return mKeyboard;
+    }
+
+    @Nullable
+    @Override
+    public CandidatesResult getCandidates(String aText) {
+        return null;
+    }
+
+    @Override
+    public String getKeyboardTitle() {
+        return StringUtils.getStringByLocale(mContext, R.string.settings_language_french, getLocale());
+    }
+
+    @Override
+    public Locale getLocale() {
+        return Locale.FRENCH;
+    }
+}
