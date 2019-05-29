4337177ed0af550cae5781be3871185b4a3d30b4
Randall E. Barker <simstorm@mac.com>
2019-05-29 15:55:33 -0700

    Update upload_symbols.sh to use new script for finding native lib (#1278)

--- tools/taskcluster/upload_symbols.sh
+++ tools/taskcluster/upload_symbols.sh
@@ -2,10 +2,5 @@
 git clone git@github.com:MozillaReality/symbolgenerator.git tools/taskcluster/symbols && cd tools/taskcluster/symbols && git rebase origin/master && cd ../../..
 
 export PYTHONPATH="tools/taskcluster/symbols/third_party/python/redo:tools/taskcluster/symbols/third_party/python/requests"
-export OBJCOPY="tools/taskcluster/symbols/bin/arm-linux-androideabi-objcopy"
 
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/googlevrArm/release/obj/armeabi-v7a/libnative-lib.so googlevr
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/noapiArm/release/obj/armeabi-v7a/libnative-lib.so noapi
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/wavevrArm/release/obj/armeabi-v7a/libnative-lib.so wavevr
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/svrArm/release/obj/armeabi-v7a/libnative-lib.so svr
-python tools/taskcluster/symbols/symbolstore.py -c -s . tools/taskcluster/symbols/bin/dump_syms tools/taskcluster/symbols/crashreporter/crashreporter-symbols ./app/build/intermediates/cmake/oculusvrArm/release/obj/armeabi-v7a/libnative-lib.so oculusvr
+python tools/taskcluster/symbols/find_symbols.py
