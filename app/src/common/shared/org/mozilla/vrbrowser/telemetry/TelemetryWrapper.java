b9e4984fabcf4ebf13c9a963203df56a53d78484
Imanol Fernandez <mortimergoro@gmail.com>
2019-07-05 16:45:27 +0200

    Re-enable  RECEIVE_BOOT_COMPLETED permission on all platforms but Quest. (#1359)
    * Re-enable  RECEIVE_BOOT_COMPLETED permission on all platforms but Quest.
    
    * Use different schedulers depending if RECEIVE_BOOT_COMPLETED permission is available.

--- app/src/common/shared/org/mozilla/vrbrowser/telemetry/TelemetryWrapper.java
+++ app/src/common/shared/org/mozilla/vrbrowser/telemetry/TelemetryWrapper.java
@@ -15,6 +15,8 @@ import org.mozilla.telemetry.measurement.SearchesMeasurement;
 import org.mozilla.telemetry.net.TelemetryClient;
 import org.mozilla.telemetry.ping.TelemetryCorePingBuilder;
 import org.mozilla.telemetry.ping.TelemetryMobileEventPingBuilder;
+import org.mozilla.telemetry.schedule.TelemetryScheduler;
+import org.mozilla.telemetry.schedule.jobscheduler.JobSchedulerTelemetryScheduler;
 import org.mozilla.telemetry.serialize.JSONPingSerializer;
 import org.mozilla.telemetry.storage.FileTelemetryStorage;
 import org.mozilla.vrbrowser.BuildConfig;
@@ -97,8 +99,13 @@ public class TelemetryWrapper {
             
             final JSONPingSerializer serializer = new JSONPingSerializer();
             final FileTelemetryStorage storage = new FileTelemetryStorage(configuration, serializer);
+            TelemetryScheduler scheduler;
+            if (BuildConfig.FLAVOR_platform.equals("oculusvr") || BuildConfig.FLAVOR_platform.equals("oculusvrStore")) {
+                scheduler = new FxRTelemetryScheduler();
+            } else {
+                scheduler = new JobSchedulerTelemetryScheduler();
+            }
             final TelemetryClient client = new TelemetryClient(new HttpURLConnectionClient());
-            final FxRTelemetryScheduler scheduler = new FxRTelemetryScheduler();
 
             TelemetryHolder.set(new Telemetry(configuration, storage, client, scheduler)
                     .addPingBuilder(new TelemetryCorePingBuilder(configuration))
