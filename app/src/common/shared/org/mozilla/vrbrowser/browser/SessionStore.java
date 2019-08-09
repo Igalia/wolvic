7db79f9a51296ac259f58e1280626c206b50bea2
Randall E. Barker <simstorm@mac.com>
2019-08-08 19:31:55 -0700

    Strip GeckoView logs from release builds. (#1539)

--- app/src/common/shared/org/mozilla/vrbrowser/browser/SessionStore.java
+++ app/src/common/shared/org/mozilla/vrbrowser/browser/SessionStore.java
@@ -767,6 +767,8 @@ public class SessionStore implements ContentBlocking.Delegate, GeckoSession.Navi
             out.write("pref(\"media.webspeech.synth.enabled\", false);\n".getBytes());
             // Prevent autozoom when giving a form field focus.
             out.write("pref(\"formhelper.autozoom\", false);\n".getBytes());
+            String geckoLogLevel = BuildConfig.DEBUG ? "Debug" : "Warn";
+            out.write(("pref(\"geckoview.logging\", \"" + geckoLogLevel + "\");\n").getBytes());
             // Uncomment this to enable WebRender. WARNING NOT READY FOR USAGE.
             // out.write("pref(\"gfx.webrender.all\", true);\n".getBytes());
             int msaa = SettingsStore.getInstance(aContext).getMSAALevel();
