9cf390828060f446c0bd92aa167711c380b675f1
Randall E. Barker <simstorm@mac.com>
2019-08-19 13:04:14 -0700

    Pin third_party for taskcluster builds (#1593)

--- tools/taskcluster/get_third_party.sh
+++ tools/taskcluster/get_third_party.sh
@@ -5,3 +5,4 @@ cp tools/ssh/config ~/.ssh
 python tools/taskcluster/fetch_secret.py -s project/firefoxreality/github-deploy-key -o ~/.ssh/deploymentkey_rsa -n key
 chmod go-rwx ~/.ssh/deploymentkey_rsa
 git clone git@github.com:MozillaReality/FirefoxReality-android-third-party.git third_party
+pushd third_party && git co -b target-branch `cat ../third_party_hash` && popd
