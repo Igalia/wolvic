a60414834fe1ff91125f11978dfc97f83828f4f2
Randall E. Barker <simstorm@mac.com>
2020-04-16 00:12:25 -0700

    Ensure UI Hardware rending is only disabled on the tray and spinner. (#3172)
    Fixes #3148

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/UISurfaceTextureRenderer.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/UISurfaceTextureRenderer.java
@@ -45,6 +45,7 @@ public class UISurfaceTextureRenderer {
         mTextureWidth = aWidth;
         mTextureHeight = aHeight;
         mSurface = aSurface;
+        mIsHardwareAccelerationEnabled = sUseHardwareAcceleration;
     }
 
     void resize(int aWidth, int aHeight) {
