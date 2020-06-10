6e09ac7d591398ee51115d34eb09c4e66f01dfa4
Randall E. Barker <simstorm@mac.com>
2020-06-10 08:27:07 -0700

    create empty gh-pages branch

--- app/src/common/shared/org/mozilla/vrbrowser/ui/callbacks/DownloadsContextMenuCallback.java
+++
@@ -1,7 +0,0 @@
-package org.mozilla.vrbrowser.ui.callbacks;
-
-import org.mozilla.vrbrowser.ui.widgets.menus.library.DownloadsContextMenuWidget;
-
-public interface DownloadsContextMenuCallback extends LibraryContextMenuCallback {
-    void onDelete(DownloadsContextMenuWidget.DownloadsContextMenuItem item);
-}
--- app/src/common/shared/org/mozilla/vrbrowser/ui/callbacks/PermissionSiteItemCallback.java
+++
@@ -1,9 +0,0 @@
-package org.mozilla.vrbrowser.ui.callbacks;
-
-import androidx.annotation.NonNull;
-
-import org.mozilla.vrbrowser.db.SitePermission;
-
-public interface PermissionSiteItemCallback {
-    void onDelete(@NonNull SitePermission item);
-}
