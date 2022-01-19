f449a04bf3314d25d624349ad3c226f7fb91ea9c
Imanol Fernandez <ifernandez@igalia.com>
2022-01-19 16:33:42 +0100

    Gecko update compatibility

--- app/src/common/shared/com/igalia/wolvic/browser/content/TrackingProtectionStore.java
+++ app/src/common/shared/com/igalia/wolvic/browser/content/TrackingProtectionStore.java
@@ -16,7 +16,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.mozilla.geckoview.ContentBlocking;
 import org.mozilla.geckoview.ContentBlockingController;
-import org.mozilla.geckoview.ContentBlockingController.ContentBlockingException;
 import org.mozilla.geckoview.GeckoRuntime;
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.VRBrowserActivity;
@@ -100,16 +99,7 @@ public class TrackingProtectionStore implements DefaultLifecycleObserver,
         public void onChanged(List<SitePermission> sitePermissions) {
             if (sitePermissions != null) {
                 mSitePermissions = sitePermissions;
-
-                // Restore the site list on the permissions storage notification
-                if (mIsFirstUpdate) {
-                    List<ContentBlockingException> exceptions = sitePermissions
-                            .stream()
-                            .map(TrackingProtectionStore::toContentBlockingException)
-                            .collect(Collectors.toList());
-                    mContentBlockingController.restoreExceptionList(exceptions);
-                    mIsFirstUpdate = false;
-                }
+                mIsFirstUpdate = false;
             }
         }
     };
@@ -121,33 +111,15 @@ public class TrackingProtectionStore implements DefaultLifecycleObserver,
     }
 
     public void contains(@NonNull Session session, Function<Boolean, Void> onResult) {
-        mContentBlockingController.checkException(session.getGeckoSession()).accept(aBoolean -> {
-            if (aBoolean != null) {
-                onResult.apply(aBoolean);
-
-            } else {
-                onResult.apply(false);
-            }
-        });
+        onResult.apply(false);
     }
 
     public void fetchAll(Function<List<SitePermission>, Void> onResult) {
         final List<SitePermission> list = new ArrayList<>();
-        mContentBlockingController.saveExceptionList().accept(contentBlockingExceptions -> {
-            if (contentBlockingExceptions != null) {
-                contentBlockingExceptions.forEach(exception -> {
-                    SitePermission site = toSitePermission(exception);
-                    if (site != null) {
-                        list.add(site);
-                    }
-                });
-            }
-            onResult.apply(list);
-        });
+        onResult.apply(list);
     }
 
     public void add(@NonNull Session session) {
-        mContentBlockingController.addException(session.getGeckoSession());
         mListeners.forEach(listener -> listener.onExcludedTrackingProtectionChange(
                 session.getCurrentUri(),
                 true,
@@ -156,7 +128,6 @@ public class TrackingProtectionStore implements DefaultLifecycleObserver,
     }
 
     public void remove(@NonNull Session session) {
-        mContentBlockingController.removeException(session.getGeckoSession());
         mListeners.forEach(listener -> listener.onExcludedTrackingProtectionChange(
                 session.getCurrentUri(),
                 false,
@@ -165,10 +136,6 @@ public class TrackingProtectionStore implements DefaultLifecycleObserver,
     }
 
     public void remove(@NonNull SitePermission permission) {
-        ContentBlockingException exception = toContentBlockingException(permission);
-        if (exception != null) {
-            mContentBlockingController.removeException(exception);
-        }
         mListeners.forEach(listener -> listener.onExcludedTrackingProtectionChange(
                 permission.url,
                 false,
@@ -177,64 +144,9 @@ public class TrackingProtectionStore implements DefaultLifecycleObserver,
     }
 
     public void removeAll() {
-        // We can't use clearExceptionList as that clears also the private browsing exceptions
-        mSitePermissions.forEach(permission -> {
-            ContentBlockingException exception = toContentBlockingException(permission);
-            if (exception != null) {
-                mContentBlockingController.removeException(exception);
-            }
-            mListeners.forEach(listener -> listener.onExcludedTrackingProtectionChange(
-                    permission.url,
-                    false,
-                    false));
-        });
-        saveExceptions();
     }
 
     private void saveExceptions() {
-        mRuntime.getContentBlockingController().saveExceptionList().accept(contentBlockingExceptions -> {
-            mViewModel.deleteAll(SITE_PERMISSION_TRACKING);
-            if (contentBlockingExceptions != null) {
-                contentBlockingExceptions.forEach(exception -> {
-                    SitePermission permission = toSitePermission(exception);
-                    if (permission != null) {
-                        mViewModel.insertSite(permission);
-                    }
-                });
-            }
-        });
-    }
-
-    @Nullable
-    private static SitePermission toSitePermission(@NonNull ContentBlockingException exception) {
-        try {
-            JSONObject json = exception.toJson();
-            return new SitePermission(
-                    json.getString("uri"),
-                    json.getString("principal"),
-                    SitePermission.SITE_PERMISSION_TRACKING);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    @Nullable
-    private static ContentBlockingException toContentBlockingException(@NonNull SitePermission permission) {
-        try {
-            JSONObject json = new JSONObject();
-            json.put("uri", permission.url);
-            json.put("principal", permission.principal);
-
-            return ContentBlockingException.fromJson(json);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     private void setTrackingProtectionLevel(int level) {
