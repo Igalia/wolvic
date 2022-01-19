0a9ca17590db25da22283c618c50c3a1be0a125a
Imanol Fernandez <ifernandez@igalia.com>
2022-01-19 16:36:06 +0100

    New environment

--- app/src/common/shared/com/igalia/wolvic/browser/SettingsStore.java
+++ app/src/common/shared/com/igalia/wolvic/browser/SettingsStore.java
@@ -93,7 +93,7 @@ public class SettingsStore {
     public final static int MAX_WINDOW_HEIGHT_DEFAULT = 1200;
     public final static int POINTER_COLOR_DEFAULT_DEFAULT = Color.parseColor("#FFFFFF");
     public final static int SCROLL_DIRECTION_DEFAULT = 0;
-    public final static String ENV_DEFAULT = "offworld";
+    public final static String ENV_DEFAULT = "wolvic";
     public final static int MSAA_DEFAULT_LEVEL = 0;
     public final static boolean AUDIO_ENABLED = false;
     public final static float CYLINDER_DENSITY_ENABLED_DEFAULT = 4680.0f;
