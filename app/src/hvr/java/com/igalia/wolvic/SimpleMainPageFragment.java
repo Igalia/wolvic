5560c35129db0bdb4c9ad340921274a58ecc8c18
Felipe Erias <felipeerias@igalia.com>
2022-05-03 14:00:59 +0900

    Link to wolvic.com in the simple main page

--- app/src/hvr/java/com/igalia/wolvic/SimpleMainPageFragment.java
+++ app/src/hvr/java/com/igalia/wolvic/SimpleMainPageFragment.java
@@ -3,11 +3,14 @@ package com.igalia.wolvic;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 
 // A simple content for the main page, just displaying a message to put on the VR glasses.
@@ -16,6 +19,16 @@ public class SimpleMainPageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.simple_main_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_open_web).setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.base_url)));
+            startActivity(intent);
+        });
     }
 
     @Override
