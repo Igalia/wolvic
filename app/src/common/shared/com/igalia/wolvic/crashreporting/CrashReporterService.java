63f474a56f35d1f12afc019e5bcfc19cc5495127
Sergio Villar Senin <svillar@igalia.com>
2022-06-22 12:23:41 +0200

    Revert "Handle the crash reporting service work in the UI thread"
    This reverts commit 577f495515851be0f98ef2e541b8d8bd1762c6ea.
    
    It was causing crashes in the crash reporting service because it must
    be run in a separate process. We still have to look for a fix for the case
    of the crash reporting service being triggered before having a valid
    GeckoView previously instantiated.
    
     E AndroidRuntime: java.lang.IllegalArgumentException: Crash handler service must run in a separate process
     E AndroidRuntime: 	at org.mozilla.geckoview.GeckoRuntime.init(GeckoRuntime.java:7)
     E AndroidRuntime: 	at org.mozilla.geckoview.GeckoRuntime.create(GeckoRuntime.java:6)
     E AndroidRuntime: 	at com.igalia.wolvic.browser.api.impl.RuntimeImpl.<init>(RuntimeImpl.java:31)
     E AndroidRuntime: 	at com.igalia.wolvic.browser.api.WFactory.createRuntime(WFactory.java:1)
     E AndroidRuntime: 	at com.igalia.wolvic.browser.engine.EngineProvider.getOrCreateRuntime(EngineProvider.kt:29)
     E AndroidRuntime: 	at com.igalia.wolvic.crashreporting.-$$Lambda$CrashReporterService$R99Aj_wMsC-FmOj1dvrAOjC9xFY.run(lambda:3)
     E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:883)
     E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:100)
     E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:214)
     E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:7391)
     E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
     E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:492)
     E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:936)

--- app/src/common/shared/com/igalia/wolvic/crashreporting/CrashReporterService.java
+++ app/src/common/shared/com/igalia/wolvic/crashreporting/CrashReporterService.java
@@ -1,7 +1,5 @@
 package com.igalia.wolvic.crashreporting;
 
-import static org.mozilla.gecko.util.ThreadUtils.runOnUiThread;
-
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
@@ -61,7 +59,8 @@ public class CrashReporterService extends JobIntentService {
         return files;
     }
 
-    private void onHandleWorkInternal(@NonNull Intent intent) {
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
         String action = intent.getAction();
         WRuntime.CrashReportIntent crash = EngineProvider.INSTANCE.getOrCreateRuntime(getBaseContext()).getCrashReportIntent();
         if (crash.action_crashed.equals(action)) {
@@ -135,11 +134,6 @@ public class CrashReporterService extends JobIntentService {
         }
 
         Log.d(LOGTAG, "Crash reporter job finished");
-    }
-
-    @Override
-    protected void onHandleWork(@NonNull Intent intent) {
-        runOnUiThread(() -> onHandleWorkInternal(intent));
     }
 
     public static void submitCaughtException(@NonNull Exception exception) {
