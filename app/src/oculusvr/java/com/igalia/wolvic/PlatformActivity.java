059bb759e3263368913744aa10772c0262e96d8f
Felipe Erias <felipeerias@igalia.com>
2022-08-31 19:59:09 +0900

    Clean up oculusvr/PlatformActivity

--- app/src/oculusvr/java/com/igalia/wolvic/PlatformActivity.java
+++ app/src/oculusvr/java/com/igalia/wolvic/PlatformActivity.java
@@ -7,14 +7,11 @@ package com.igalia.wolvic;
 
 import android.Manifest;
 import android.app.NativeActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
-
-import androidx.annotation.NonNull;
 
 import com.igalia.wolvic.utils.SystemUtils;
 
