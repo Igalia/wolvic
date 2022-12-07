e292d5ff83090a12ebbccefc17a3c2d4cbad635f
Ana2k <anushkachakra@gmail.com>
2022-12-07 16:08:46 +0530

    content resolver copy files using content uris

--- app/src/common/shared/com/igalia/wolvic/downloads/DownloadsManager.java
+++ app/src/common/shared/com/igalia/wolvic/downloads/DownloadsManager.java
@@ -2,23 +2,32 @@ package com.igalia.wolvic.downloads;
 
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 import android.webkit.URLUtil;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.utils.UrlUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
@@ -96,6 +105,7 @@ public class DownloadsManager {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     public void startDownload(@NonNull DownloadJob job) {
         if (!URLUtil.isHttpUrl(job.getUri()) && !URLUtil.isHttpsUrl(job.getUri())) {
             notifyDownloadError(mContext.getString(R.string.download_error_protocol), job.getFilename());
@@ -104,7 +114,6 @@ public class DownloadsManager {
 
         Uri url = Uri.parse(job.getUri());
         DownloadManager.Request request = new DownloadManager.Request(url);
-
         request.setTitle(job.getTitle());
         request.setDescription(job.getDescription());
         request.setMimeType(job.getContentType());
@@ -113,8 +122,7 @@ public class DownloadsManager {
 
         if (job.getOutputPath() == null) {
             try {
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, job.getFilename());
-                // :request.setDestinationInExternalFilesDir(mContext, Environment.DIRECTORY_DOWNLOADS, job.getFilename());
+                request.setDestinationInExternalFilesDir(mContext, Environment.DIRECTORY_DOWNLOADS, job.getFilename());
             } catch (IllegalStateException e) {
                 e.printStackTrace();
                 notifyDownloadError(mContext.getString(R.string.download_error_output), job.getFilename());
@@ -135,7 +143,6 @@ public class DownloadsManager {
             scheduleUpdates();
         }
     }
-
 
     public void removeDownload(long downloadId, boolean deleteFiles) {
         Download download = getDownload(downloadId);
@@ -225,7 +232,13 @@ public class DownloadsManager {
                 if (c != null) {
                     if (c.moveToFirst()) {
                         notifyDownloadsUpdate();
-                        notifyDownloadCompleted(Download.from(c));
+                        Download download = Download.from(c);
+                        notifyDownloadCompleted(download);
+
+                        // Copy the downloaded file to the public Downloads folder.
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            copyToDownloadsFolder(download);
+                        }
                     }
                     c.close();
                 }
@@ -233,10 +246,85 @@ public class DownloadsManager {
         }
     };
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void copyToDownloadsFolder(Download download) {
+        Log.e(LOGTAG, "************************ copyToDownloadsFolder ************************");
+        Log.e(LOGTAG, "DISPLAY_NAME : " + download.getFilename());
+        Log.e(LOGTAG, "EXTERNAL_CONTENT_URI : " + MediaStore.Downloads.EXTERNAL_CONTENT_URI);
+        Log.e(LOGTAG, "SIZE : " + download.getSizeBytes());
+        Log.e(LOGTAG, "OutputFilePath : " + download.getOutputFilePath());
+
+        // Create a new entry for the Downloads table in MediaStore.
+        ContentResolver contentResolver = mContext.getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Downloads.DISPLAY_NAME, download.getFilename());
+        contentValues.put(MediaStore.Downloads.SIZE, download.getSizeBytes());
+        contentValues.put(MediaStore.Downloads.DATE_MODIFIED, download.getLastModified());
+        contentValues.put(MediaStore.Downloads.DOWNLOAD_URI, download.getUri());
+
+        // TODO There are several ways to obtain the MIME type. But how do we pick the right one?
+
+        String mimeFromExtension = null;
+        String extension = MimeTypeMap.getFileExtensionFromUrl(download.getOutputFilePath());
+        if (extension != null) {
+            mimeFromExtension = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        }
+        Log.e(LOGTAG, "MIME type from extension : " + mimeFromExtension);
+
+        File downloadedFile = new File(download.getOutputFilePath());
+        Uri downloadedFileUri = Uri.fromFile(downloadedFile);
+        String mimeFromFile = contentResolver.getType(downloadedFileUri);
+        Log.e(LOGTAG, "MIME type from file " + downloadedFileUri + " : " + mimeFromFile);
+
+        String mimeFromDownload = download.getMediaType();
+        Log.e(LOGTAG, "MIME type from download : " + download.getMediaType());
+
+        // TODO : find out which of the possible MIME types is the most specific.
+        if (mimeFromExtension != null) {
+            contentValues.put(MediaStore.Downloads.MIME_TYPE, mimeFromExtension);
+        } else if (mimeFromFile != null) {
+            contentValues.put(MediaStore.Downloads.MIME_TYPE, mimeFromFile);
+        } else {
+            contentValues.put(MediaStore.Downloads.MIME_TYPE, mimeFromDownload);
+        }
+
+        // This flag indicates that the file is not yet ready.
+        contentValues.put(MediaStore.Downloads.IS_PENDING, 1);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            contentValues.put(MediaStore.Downloads.IS_DRM, 0);
+        }
+
+        Log.e(LOGTAG, "Inserting : " + contentValues);
+
+        Uri uri = contentResolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues);
+
+        Log.e(LOGTAG, "Inserted URI : " + uri);
+
+        // This URI can be accessed from the console:
+        //     adb shell content query --uri content://media/external/downloads/362
+
+        try {
+            Log.e(LOGTAG, "Source file : " + downloadedFile);
+
+            Log.e(LOGTAG, "Copying to : " + uri);
+            Files.copy(downloadedFile.toPath(), contentResolver.openOutputStream(uri));
+
+            // The file is finally ready.
+            Log.e(LOGTAG, "Updating IS_PENDING");
+            contentValues.put(MediaStore.Downloads.IS_PENDING, 0);
+            contentResolver.update(uri, contentValues, null, null);
+
+            Log.e(LOGTAG, "************************ Done! ************************");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void notifyDownloadsUpdate() {
         List<Download> downloads = getDownloads();
         int filter = Download.RUNNING | Download.PAUSED | Download.PENDING;
-        boolean activeDownloads = downloads.stream().filter(d -> (d.getStatus() & filter) != 0).count()  > 0;
+        boolean activeDownloads = downloads.stream().filter(d -> (d.getStatus() & filter) != 0).count() > 0;
         mListeners.forEach(listener -> listener.onDownloadsUpdate(downloads));
         if (!activeDownloads) {
             stopUpdates();
