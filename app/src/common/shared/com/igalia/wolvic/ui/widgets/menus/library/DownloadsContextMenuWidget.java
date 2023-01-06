c0eb7540f9db68309a99da17239f0fc15c108d9a
Felipe Erias <felipeerias@igalia.com>
2023-01-06 17:41:59 +0800

    Simplify the Library menu code by using EnumSet to encapsulate the information about additional menu items.

--- app/src/common/shared/com/igalia/wolvic/ui/widgets/menus/library/DownloadsContextMenuWidget.java
+++ app/src/common/shared/com/igalia/wolvic/ui/widgets/menus/library/DownloadsContextMenuWidget.java
@@ -7,11 +7,22 @@ import androidx.annotation.NonNull;
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.ui.callbacks.DownloadsContextMenuCallback;
 
+import java.util.EnumSet;
+
 public class DownloadsContextMenuWidget extends LibraryContextMenuWidget {
 
     public DownloadsContextMenuWidget(Context aContext, LibraryContextMenuItem item,
                                       boolean canOpenWindows, boolean canCopyToContentUri) {
-        super(aContext, item, canOpenWindows, false, canCopyToContentUri);
+        super(aContext, item, getAdditionalActions(canOpenWindows, canCopyToContentUri));
+    }
+
+    private static EnumSet<Action> getAdditionalActions(boolean canOpenWindows, boolean canCopyToContentUri) {
+        EnumSet<Action> additionalActions = EnumSet.noneOf(Action.class);
+        if (canOpenWindows)
+            additionalActions.add(Action.OPEN_WINDOW);
+        if (canCopyToContentUri)
+            additionalActions.add(Action.CAN_COPY);
+        return additionalActions;
     }
 
     public static class DownloadsContextMenuItem extends LibraryContextMenuItem {
@@ -30,8 +41,9 @@ public class DownloadsContextMenuWidget extends LibraryContextMenuWidget {
 
     }
 
-    protected void setupCustomMenuItems(boolean canOpenWindows, boolean isBookmarked, boolean canCopyToContentUri) {
-        if (canCopyToContentUri) {
+    @Override
+    protected void setupCustomMenuItems(EnumSet<Action> additionalActions) {
+        if (additionalActions.contains(Action.CAN_COPY)) {
             mItems.add(new MenuItem(getContext().getString(
                     R.string.download_context_copy_to_content_uri),
                     R.drawable.ic_icon_file_copy,
--- app/src/common/shared/com/igalia/wolvic/ui/widgets/menus/library/HistoryContextMenuWidget.java
+++ app/src/common/shared/com/igalia/wolvic/ui/widgets/menus/library/HistoryContextMenuWidget.java
@@ -5,13 +5,26 @@ import android.content.Context;
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.ui.callbacks.HistoryContextMenuCallback;
 
+import java.util.EnumSet;
+
 public class HistoryContextMenuWidget extends LibraryContextMenuWidget {
 
     public HistoryContextMenuWidget(Context aContext, LibraryContextMenuItem item, boolean canOpenWindows, boolean isBookmarked) {
-        super(aContext, item, canOpenWindows, isBookmarked, false);
+        super(aContext, item, getAdditionalActions(canOpenWindows, isBookmarked));
     }
 
-    protected void setupCustomMenuItems(boolean canOpenWindows, boolean isBookmarked, boolean canCopyToContentUri) {
+    private static EnumSet<Action> getAdditionalActions(boolean canOpenWindows, boolean isBookmarked) {
+        EnumSet<Action> additionalActions = EnumSet.noneOf(Action.class);
+        if (canOpenWindows)
+            additionalActions.add(Action.OPEN_WINDOW);
+        if (isBookmarked)
+            additionalActions.add(Action.IS_BOOKMARKED);
+        return additionalActions;
+    }
+
+    @Override
+    protected void setupCustomMenuItems(EnumSet<Action> additionalActions) {
+        boolean isBookmarked = additionalActions.contains(Action.IS_BOOKMARKED);
         mItems.add(new MenuItem(getContext().getString(
                 isBookmarked ? R.string.history_context_remove_bookmarks : R.string.history_context_add_bookmarks),
                 isBookmarked ? R.drawable.ic_icon_bookmarked_active : R.drawable.ic_icon_bookmarked,
