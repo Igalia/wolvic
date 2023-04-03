37948806f7ac7934a030a71b6f7ecd446fdd0bc7
Miyoung Shin <124305083+mshin-wolvic@users.noreply.github.com>
2023-04-03 11:25:11 +0900

    Support touch events & key events including IME (#592)
    * Support touch events & key events including IME
    
    - Add ContentView managed by WebContents to implement PanZoomCrontrollerImpl for touch events.
    - Use ImeAdapter & ContentView that communicate native <-> java to implement TextInputImpl for key events including IME.
    
    ---------
    
    Co-authored-by: Miyoung Shin <myid.shin@igalia.com>

--- app/src/common/chromium/com/igalia/wolvic/browser/api/impl/PanZoomControllerImpl.java
+++ app/src/common/chromium/com/igalia/wolvic/browser/api/impl/PanZoomControllerImpl.java
@@ -1,25 +1,31 @@
 package com.igalia.wolvic.browser.api.impl;
 
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 
 import com.igalia.wolvic.browser.api.WPanZoomController;
 
-public class PanZoomCrontrollerImpl implements WPanZoomController {
+public class PanZoomControllerImpl implements WPanZoomController {
     SessionImpl mSession;
 
-    public PanZoomCrontrollerImpl(SessionImpl session) {
+    public PanZoomControllerImpl(SessionImpl session) {
         mSession = session;
     }
 
     @Override
     public void onTouchEvent(@NonNull MotionEvent event) {
-        // TODO: implement
+        getContentView().dispatchTouchEvent(event);
     }
 
     @Override
     public void onMotionEvent(@NonNull MotionEvent event) {
-        // TODO: implement
+        getContentView().dispatchGenericMotionEvent(event);
+    }
+
+    private ViewGroup getContentView() {
+        assert mSession != null;
+        return mSession.getContentView();
     }
 }
--- app/src/common/chromium/org/chromium/components/embedder_support/view/WolvicContentRenderView.java
+++ app/src/common/chromium/org/chromium/components/embedder_support/view/WolvicContentRenderView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.view.Surface;
 
+import androidx.annotation.Nullable;
+
+import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
 
 public class WolvicContentRenderView extends ContentViewRenderView {
@@ -39,4 +42,7 @@ public class WolvicContentRenderView extends ContentViewRenderView {
         assert mNativeContentViewRenderView != 0;
         ContentViewRenderViewJni.get().surfaceDestroyed(mNativeContentViewRenderView, this);
     }
+
+    @Nullable
+    public WebContents getCurrentWebContents() { return mWebContents; }
 }
