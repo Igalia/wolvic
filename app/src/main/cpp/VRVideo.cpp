cdfb67dc233e8eed84b4295ef43e3f2eb5e69d1f
kavi.kumaran <kavi.kumaran@zohocorp.com>
2023-04-06 13:27:25 +0530

    Fix issue #599: undeclared identifier 'create180LRProjectionLayer' in VRVideo.cpp

--- app/src/main/cpp/VRVideo.cpp
+++ app/src/main/cpp/VRVideo.cpp
@@ -260,6 +260,22 @@ struct VRVideo::State {
     leftEye = create180LayerToggle(equirect);
   }
 
+  void create180LRProjectionLayer() {
+    vrb::CreationContextPtr create = context.lock();
+    DeviceDelegatePtr device = deviceWeak.lock();
+    VRLayerEquirectPtr equirect = device->CreateLayerEquirect(window->GetLayer());
+    layer = equirect;
+
+    equirect->SetTextureRect(device::Eye::Left, device::EyeRect(0.0f, 0.0f, 0.5f, 1.0f));
+    equirect->SetTextureRect(device::Eye::Right, device::EyeRect(0.5f, 0.0f, 0.5f, 1.0f));
+    auto UVtransform = vrb::Matrix::Identity().Scale(vrb::Vector(2.0f, 1.0f, 1.0f)).Translate(vrb::Vector(-0.5, 0.0, 0.0));
+    equirect->SetUVTransform(device::Eye::Left, UVtransform);
+    equirect->SetUVTransform(device::Eye::Right, UVtransform);
+    equirect->SetUseSameLayerForBothEyes(false);
+
+    leftEye = create180LayerToggle(equirect);
+  }
+  
 #ifdef OPENXR
   void create180LRProjectionLayer() {
     vrb::CreationContextPtr create = context.lock();
