165ef561151bcc0980049933c873eaaebf2c00e9
Eduardo Lima <elima@filetea.me>
2023-04-21 17:56:18 +0200

    New back-navigation gesture using index-pinch when left hand faces head
    Current gesture for navigating back uses a pinch geature between thumb
    and ring fingertip. This gesture is far too easy to trigger involuntarily
    and results in many usability issues (and user complains).
    
    While we are still defining a comprehensive solution for all hand-interactions
    in Wolvic, this change serves as a short/mid term fix. It replaces the
    existing gesture by a new one: a normal pinch (thumb + index fingertip)
    with left hand facing the head. This is inspired by Oculus behavior, where
    a hamburger icon is shown when left hand is facing head. This specific
    icon is already generalized on the controllers to produce a navigate-back
    action.
    
    A circled, hamburger menu icon is shown between thumb and index's fingertips,
    when the left hand is facing the head. As the user performs a thumb+index
    pinch, the icon reduces its size up to 50%, when the pinch is closed, the
    button is hidden, and a navigate-back action is triggered.
    
    It has been tested on Quest2 and Pico4 devices.

--- app/src/main/cpp/ControllerDelegate.h
+++ app/src/main/cpp/ControllerDelegate.h
@@ -21,6 +21,17 @@ typedef std::shared_ptr<ControllerDelegate> ControllerDelegatePtr;
 
 enum class ControllerMode { None, Device, Hand };
 
+
+
+// Follows XR_EXT_hand_tracking' XrHandJointEXT enum
+enum class HandTrackingJoints {
+  Palm, Wrist,
+  ThumbMetacarpal, ThumbProximal, ThumbDistal, ThumbTip,
+  IndexMetacarpal, IndexProximal, IndexIntermediate, IndexDistal, IndexTip,
+  MiddleMetacarpal, MiddleProximal, MiddleIntermediate, MiddleDistal, MiddleTip,
+  RingMetacarpal, RingProximal, RingIntermediate, RingDistal, RingTip,
+  LittleMetacarpal, LittleProximal, LittleIntermediate, LittleDistal, LittleTip };
+
 class ControllerDelegate {
 public:
   enum Button {
@@ -68,6 +79,7 @@ public:
   virtual void SetGazeModeIndex(const int32_t aControllerIndex) = 0;
   virtual void SetHandJointLocations(const int32_t aControllerIndex, std::vector<vrb::Matrix>& jointTransforms) = 0;
   virtual void SetAimEnabled(const int32_t aControllerIndex, bool aEnabled = true) = 0;
+  virtual void SetLeftHandActionEnabled(const int32_t aControllerIndex, bool aEnabled = false) = 0;
   virtual void SetMode(const int32_t aControllerIndex, ControllerMode aMode = ControllerMode::None) = 0;
   virtual void SetPinchFactor(const int32_t aControllerIndex, float aFactor = 1.0f) = 0;
 protected:
