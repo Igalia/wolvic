edee326e451718fc06d84250ac66a0273265d408
Sergio Villar Senin <svillar@igalia.com>
2023-05-10 18:20:00 +0200

    Add the right WebXR input profiles for Pico4 and Pico4E
    They have been added to the WebXR repo here
    https://github.com/immersive-web/webxr-input-profiles/blob/main/packages/registry/profiles/pico/pico-4.json
    
    Gecko didn't have support for them, so we had to add them to our
    Gecko branch too
    https://github.com/Igalia/gecko-dev/commit/13155ba80b0afb3b6dd8e7ed8dcbede2decde8bb.

--- app/src/main/cpp/ExternalVR.cpp
+++ app/src/main/cpp/ExternalVR.cpp
@@ -245,7 +245,7 @@ mozilla::gfx::VRControllerType GetVRControllerTypeByDevice(device::DeviceType aT
       result = mozilla::gfx::VRControllerType::PicoNeo2;
       break;
     case device::PicoXR:
-      result = mozilla::gfx::VRControllerType::PicoNeo2;
+      result = mozilla::gfx::VRControllerType::Pico4;
       break;
     case device::UnknownType:
     default:
