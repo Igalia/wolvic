ef2007dfd2235b64a876bef1de57821e8c4fada4
Javier Fernández García-Boente <jfernandez@igalia.com>
2023-05-23 17:06:32 +0200

    Preliminary approach

--- app/src/spaces/java/com/igalia/wolvic/PlatformActivity.java
+++ app/src/spaces/java/com/igalia/wolvic/PlatformActivity.java
@@ -5,15 +5,59 @@
 
 package com.igalia.wolvic;
 
+import android.app.ActivityOptions;
 import android.app.NativeActivity;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
-import android.view.WindowManager;
 
 import com.igalia.wolvic.utils.SystemUtils;
 
 public class PlatformActivity extends NativeActivity {
+
+    private static final String controllerActivity = "com.qualcomm.snapdragon.spaces.hostcontroller.ControllerActivity";
+    static String LOGTAG = SystemUtils.createLogtag(PlatformActivity.class);
+
+    private boolean getMetaDataFlag(String key) {
+        boolean flag = false;
+        try {
+            ApplicationInfo app = this.getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = app.metaData;
+
+            flag = bundle.getBoolean("com.qualcomm.snapdragon.spaces." + key, false);
+        } catch (Exception e) {
+            Log.e(LOGTAG, "Can't get meta data info to retrieve flag for: " + key);
+            e.printStackTrace();
+        }
+
+        return flag;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.v(LOGTAG, "Application was started with intent flags: " + getIntent().getFlags());
+        try {
+            Class.forName(controllerActivity);
+            tryStartControllerActivity();
+        } catch(ClassNotFoundException e) {
+            Log.e(LOGTAG, "There was no Spaces Controller included in the project! No controller to launch therefore.");
+        }
+    }
+    private void tryStartControllerActivity() {
+          Intent controllerIntent = new Intent(getIntent());
+          Bundle startupBundle = null;
+          controllerIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+          controllerIntent.setClassName(this, controllerActivity);
+          startupBundle = ActivityOptions.makeBasic().setLaunchDisplayId(0).toBundle();
+          controllerIntent.putExtras(startupBundle);
+
+          startActivity(controllerIntent, startupBundle);
+    }
 
     public static boolean filterPermission(final String aPermission) {
         // Dummy implementation.
