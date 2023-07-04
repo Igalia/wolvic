1860f56470fa416e66238783282ac4a29f1fa166
Mike Ratcliffe <michael@ratcliffefamily.org>
2023-07-04 14:49:42 +0100

    Gracefully handle SocketTimeout when MLS not available

--- app/src/common/shared/com/igalia/wolvic/geolocation/GeolocationWrapper.kt
+++ app/src/common/shared/com/igalia/wolvic/geolocation/GeolocationWrapper.kt
@@ -1,8 +1,10 @@
 package com.igalia.wolvic.geolocation
 
 import android.content.Context
+import android.util.Log
 import com.igalia.wolvic.browser.SettingsStore
 import com.igalia.wolvic.browser.engine.EngineProvider
+import com.igalia.wolvic.utils.SystemUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -10,9 +12,11 @@ import kotlinx.coroutines.future.future
 import kotlinx.coroutines.launch
 import mozilla.components.service.location.LocationService
 import mozilla.components.service.location.MozillaLocationService
+import java.net.SocketTimeoutException
 import java.util.concurrent.CompletableFuture
 
 object GeolocationWrapper {
+    private val LOGTAG = SystemUtils.createLogtag(GeolocationWrapper::class.java)
 
     fun update(context: Context) {
         val locationService = MozillaLocationService(
@@ -21,9 +25,13 @@ object GeolocationWrapper {
                 com.igalia.wolvic.BuildConfig.MLS_TOKEN
         )
         CoroutineScope(Dispatchers.IO).launch {
-            locationService.fetchRegion(true)?.run {
-                val data: GeolocationData = GeolocationData.create(countryCode, countryName)
-                SettingsStore.getInstance(context).geolocationData = data.toString()
+            try {
+                locationService.fetchRegion(true)?.run {
+                    val data: GeolocationData = GeolocationData.create(countryCode, countryName)
+                    SettingsStore.getInstance(context).geolocationData = data.toString()
+                }
+            } catch (e: SocketTimeoutException) {
+                Log.w(LOGTAG, "The Mozilla Location Service (MLS) is not available.")
             }
         }
     }
