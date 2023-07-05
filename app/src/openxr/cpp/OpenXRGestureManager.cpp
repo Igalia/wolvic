a09c1e6e5751815ed1262d551a9a6aec7e79182f
Sergio Villar Senin <svillar@igalia.com>
2023-07-05 14:38:54 +0200

    [OpenXR] Add system gesture recognition to GestureManager
    Many devices trigger some system event whenever the palms
    face the head. For example Meta takes control over the hands
    and show a menu. Pico also starts a timer to launch a menu.
    We do use that gesture also to trigger navigation back or
    to exit the application in other devices.
    
    This code that was in OpenXRInputSource is properly placed
    now in the GestureManager so that it does not interfere
    with the rest of the generic code for controllers. This
    allowed us to remove some attributes from OpenXRInputSource
    that really belong to the GestureManager.

--- app/src/openxr/cpp/OpenXRGestureManager.cpp
+++ app/src/openxr/cpp/OpenXRGestureManager.cpp
@@ -7,6 +7,26 @@
 
 namespace crow {
 
+// This threshold is used to detect when palm is facing the head,
+// and enable the left hand action gesture. The higher the threshold
+// the more aligned objects should be to be considered facing each other.
+// 0.7 is generally accepted as good for objects facing each other.
+const float kPalmHeadThreshold = 0.7;
+
+bool
+OpenXRGestureManager::palmFacesHead(const vrb::Matrix &palm, const vrb::Matrix &head) const {
+    // For the hand we take the Y axis because that corresponds to head's Z axis when
+    // the hand is in upright position facing head (the gesture we want to detect).
+#ifdef PICOXR
+    // Axis are inverted in Pico
+    auto vectorPalm = palm.MultiplyDirection({0, 0, -1});
+#else
+    auto vectorPalm = palm.MultiplyDirection({0, 1, 0});
+#endif
+    auto vectorHead = head.MultiplyDirection({0, 0, -1});
+    return vectorPalm.Dot(vectorHead) > kPalmHeadThreshold;
+}
+
 void
 OpenXRGestureManagerFBHandTrackingAim::populateNextStructureIfNeeded(XrHandJointLocationsEXT& handJointLocations) {
     memset(&mFBAimState, 0, sizeof(XrHandTrackingAimStateFB));
@@ -20,9 +40,19 @@ OpenXRGestureManagerFBHandTrackingAim::hasAim() const {
 }
 
 XrPosef
-OpenXRGestureManagerFBHandTrackingAim::aimPose(const XrHandJointLocationsEXT& handJointLocations, const XrTime predictedDisplayTime, const OpenXRHandFlags handeness, const vrb::Matrix& head) const {
+OpenXRGestureManagerFBHandTrackingAim::aimPose(const XrTime predictedDisplayTime, const OpenXRHandFlags handeness, const vrb::Matrix& head) const {
     ASSERT(hasAim());
     return mFBAimState.aimPose;
+}
+
+bool
+OpenXRGestureManagerFBHandTrackingAim::systemGestureDetected(const vrb::Matrix& palm, const vrb::Matrix& head) const {
+#ifdef PICOXR
+    // Pico does not correctly implement the SYSTEM_GESTURE_BIT_FB flags.
+    return palmFacesHead(palm, head) && !hasAim();
+#else
+    return mFBAimState.status & XR_HAND_TRACKING_AIM_SYSTEM_GESTURE_BIT_FB;
+#endif
 }
 
 OpenXRGestureManagerHandJoints::OpenXRGestureManagerHandJoints(HandJointsArray& handJoints)
@@ -36,7 +66,7 @@ OpenXRGestureManagerHandJoints::hasAim() const {
 }
 
 XrPosef
-OpenXRGestureManagerHandJoints::aimPose(const XrHandJointLocationsEXT& handJointLocations, const XrTime predictedDisplayTime, const OpenXRHandFlags handeness, const vrb::Matrix& head) const {
+OpenXRGestureManagerHandJoints::aimPose(const XrTime predictedDisplayTime, const OpenXRHandFlags handeness, const vrb::Matrix& head) const {
     ASSERT(hasAim());
     auto lookAt = [](const vrb::Vector& sourcePoint, const vrb::Vector& destPoint) -> vrb::Quaternion {
         const float EPSILON = 0.000001f;
@@ -64,7 +94,7 @@ OpenXRGestureManagerHandJoints::aimPose(const XrHandJointLocationsEXT& handJoint
         return quaternionFromAxisAndAngle(rotationAxis.Normalize(), rotationAngle);
     };
 
-    auto aimPose = handJointLocations.jointLocations[XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT].pose;
+    auto aimPose = mHandJoints[XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT].pose;
     auto pos = vrb::Vector(aimPose.position.x, aimPose.position.y, aimPose.position.z);
     float* filteredPos = mOneEuroFilterPosition->filter(predictedDisplayTime, pos.Data());
 
@@ -72,6 +102,11 @@ OpenXRGestureManagerHandJoints::aimPose(const XrHandJointLocationsEXT& handJoint
     auto q = lookAt({filteredPos[0], filteredPos[1], filteredPos[2]}, shoulder);
     aimPose.orientation = {q.x(), q.y(), q.z(), q.w() };
     return aimPose;
+}
+
+bool
+OpenXRGestureManagerHandJoints::systemGestureDetected(const vrb::Matrix& palm, const vrb::Matrix& head) const {
+    return palmFacesHead(palm, head);
 }
 
 }
