95dcf618571a60a739ad7312ec3ad05bd5af6e64
Songlin Jiang <sjiang@igalia.com>
2023-07-22 18:13:09 +0300

    Revert "Copy download to content URI"
    We no longer need these since now we download directly to
    public Downloads folder.
    
    This reverts commit adab2d3742fd5f3321a46e5cc667a4d25e0505c6.

--- app/src/common/shared/com/igalia/wolvic/ui/callbacks/DownloadsContextMenuCallback.java
+++ app/src/common/shared/com/igalia/wolvic/ui/callbacks/DownloadsContextMenuCallback.java
@@ -3,7 +3,5 @@ package com.igalia.wolvic.ui.callbacks;
 import com.igalia.wolvic.ui.widgets.menus.library.DownloadsContextMenuWidget;
 
 public interface DownloadsContextMenuCallback extends LibraryContextMenuCallback {
-    void onCopyToContentUri(DownloadsContextMenuWidget.DownloadsContextMenuItem item);
-
     void onDelete(DownloadsContextMenuWidget.DownloadsContextMenuItem item);
 }
--- app/src/common/shared/com/igalia/wolvic/ui/views/library/DownloadsView.java
+++ app/src/common/shared/com/igalia/wolvic/ui/views/library/DownloadsView.java
@@ -9,14 +9,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,9 +24,7 @@ import com.igalia.wolvic.R;
 import com.igalia.wolvic.VRBrowserActivity;
 import com.igalia.wolvic.browser.SettingsStore;
 import com.igalia.wolvic.browser.engine.SessionStore;
-import com.igalia.wolvic.browser.extensions.LocalExtension;
 import com.igalia.wolvic.databinding.DownloadsBinding;
-import com.igalia.wolvic.downloads.CopyToContentUriCallback;
 import com.igalia.wolvic.downloads.Download;
 import com.igalia.wolvic.downloads.DownloadsManager;
 import com.igalia.wolvic.telemetry.TelemetryService;
@@ -45,12 +41,13 @@ import com.igalia.wolvic.ui.widgets.menus.library.DownloadsContextMenuWidget;
 import com.igalia.wolvic.ui.widgets.menus.library.LibraryContextMenuWidget;
 import com.igalia.wolvic.ui.widgets.menus.library.SortingContextMenuWidget;
 import com.igalia.wolvic.utils.SystemUtils;
+import com.igalia.wolvic.browser.extensions.LocalExtension;
 import com.igalia.wolvic.utils.UrlUtils;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.UUID;
 
 public class DownloadsView extends LibraryView implements DownloadsManager.DownloadsListener {
 
@@ -249,9 +246,6 @@ public class DownloadsView extends LibraryView implements DownloadsManager.Downl
 
         @Override
         public void onShowContextMenu(@NonNull View view, Download item, boolean isLastVisibleItem) {
-            boolean canCopyToContentUri = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-                    && mDownloadsManager.getContentUriForDownloadedFile(item) == null;
-
             showContextMenu(
                     view,
                     new DownloadsContextMenuWidget(getContext(),
@@ -259,8 +253,7 @@ public class DownloadsView extends LibraryView implements DownloadsManager.Downl
                                     item.getOutputFileUriAsString(),
                                     item.getTitle(),
                                     item.getId()),
-                            mWidgetManager.canOpenNewWindow(),
-                            canCopyToContentUri),
+                            mWidgetManager.canOpenNewWindow()),
                     mCallback,
                     isLastVisibleItem);
         }
@@ -273,26 +266,6 @@ public class DownloadsView extends LibraryView implements DownloadsManager.Downl
         @Override
         public void onShowSortingContextMenu(@NonNull View view) {
             showSortingContextMenu(view);
-        }
-    };
-
-    private CopyToContentUriCallback mCopyToContentUriCallback = new CopyToContentUriCallback() {
-        @Override
-        public void onSuccess(Uri uri, boolean isNewUri) {
-            mWidgetManager.getFocusedWindow().showAlert(
-                    getContext().getString(R.string.download_copy_to_content_uri_success_title),
-                    getContext().getString(R.string.download_copy_to_content_uri_success_body),
-                    null);
-        }
-
-        @Override
-        public void onFailure(Download download) {
-            String errorBody = (download != null) ?
-                    getContext().getString(R.string.download_copy_to_content_uri_error_body_v1, download.getFilename()) :
-                    getContext().getString(R.string.download_copy_to_content_uri_error_body);
-
-            mWidgetManager.getFocusedWindow().showAlert(
-                    getContext().getString(R.string.download_copy_to_content_uri_error_title), errorBody, null);
         }
     };
 
@@ -336,19 +309,6 @@ public class DownloadsView extends LibraryView implements DownloadsManager.Downl
             mWidgetManager.openNewTabForeground(item.getUrl());
             TelemetryService.Tabs.openedCounter(TelemetryService.Tabs.TabSource.DOWNLOADS);
             hideContextMenu();
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.Q)
-        @Override
-        public void onCopyToContentUri(DownloadsContextMenuWidget.DownloadsContextMenuItem item) {
-            mWidgetManager.getFocusedWindow().showConfirmPrompt(
-                    getContext().getString(R.string.download_copy_to_content_uri_confirm_title),
-                    getContext().getString(R.string.download_copy_to_content_uri_confirm_body),
-                    new String[]{getContext().getString(R.string.cancel_button),
-                            getContext().getString(R.string.ok_button)},
-                    (index, isChecked) -> {
-                        mDownloadsManager.copyToContentUri(item.getDownloadsId(), mCopyToContentUriCallback);
-                    });
         }
 
         @Override
