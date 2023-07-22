13b72990dc80880f83ad193821f59476d503a49a
Songlin Jiang <sjiang@igalia.com>
2023-07-22 18:13:09 +0300

    Revert "Copy download to content URI"
    We no longer need these since now we download directly to
    public Downloads folder.
    
    This reverts commit adab2d3742fd5f3321a46e5cc667a4d25e0505c6.

--- app/src/common/shared/com/igalia/wolvic/downloads/CopyToContentUriCallback.java
+++
@@ -1,9 +0,0 @@
-package com.igalia.wolvic.downloads;
-
-import android.net.Uri;
-
-public interface CopyToContentUriCallback {
-    void onSuccess(Uri uri, boolean isNewUri);
-
-    void onFailure(Download download);
-}
