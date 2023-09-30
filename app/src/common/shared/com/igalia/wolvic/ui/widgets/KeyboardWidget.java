7b4afab4bfbcae8832bfc681c8c1824ef6173495
Songlin Jiang <sjiang@igalia.com>
2023-09-30 10:31:51 +0200

    Fix voice input when keyboard uses composing text
    We don't want to compose text again when we use
    voice input, e.g. Voice input English while keyboard
    layout is Chinese.
    
    Signed-off-by: Songlin Jiang <sjiang@igalia.com>

--- app/src/common/shared/com/igalia/wolvic/ui/widgets/KeyboardWidget.java
+++ app/src/common/shared/com/igalia/wolvic/ui/widgets/KeyboardWidget.java
@@ -1060,7 +1060,7 @@ public class KeyboardWidget extends UIWidget implements CustomKeyboardView.OnKey
 
         final String text = aText;
         final InputConnection connection = mInputConnection;
-        if (mCurrentKeyboard.usesComposingText()) {
+        if (mCurrentKeyboard.usesComposingText() && !mIsInVoiceInput) {
             postInputCommand(() -> {
                 CharSequence seq = connection.getSelectedText(0);
                 String selected = seq != null ? seq.toString() : "";
@@ -1070,7 +1070,7 @@ public class KeyboardWidget extends UIWidget implements CustomKeyboardView.OnKey
                 }
                 mComposingText += text;
             });
-        } else if (mCurrentKeyboard.usesTextOverride()) {
+        } else if (mCurrentKeyboard.usesTextOverride() || mIsInVoiceInput) {
             postInputCommand(() -> {
                 String beforeText = getTextBeforeCursor(connection);
                 String newBeforeText = mCurrentKeyboard.overrideAddText(beforeText, text);
