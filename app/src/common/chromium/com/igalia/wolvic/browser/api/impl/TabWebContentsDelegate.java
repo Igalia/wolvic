9159b88dbe08aa3c787ac8d58fa2112513306270
Sergio Villar Senin <svillar@igalia.com>
2023-11-21 13:17:12 +0100

    [Chromium] Add support for target=_blank navigations
    In these cases the renderer process creates new WebContents and
    notifies the client (Wolvic) about that. However it is still
    very difficult to accomodate that pattern with the current
    Wolvic architecture which was mainly designed to fit well with
    GeckoView API. In particular it'll be really hard to stack a
    new Session and at the same time not creating a new TabImpl
    with its own WebContents as it happens now. Needless to say,
    that requires several new objects on the chromium side to
    support that use case.
    
    However we've found a different way to do that which does not
    actually require changes in Chromium. It's based on a feature
    of Chromium that allows embedders to create their own WebContents
    even in those cases. If we declare that we will provide our own
    WebContents (by returning false in shouldCreateWebContents()
    called by WebContentsDelegateAndroid::IsWebContentsCreationOverridden)
    and then later on we don't provide any (as the default implementation
    of the delegate does) then Chromium considers that it
    should not do any window creation. So Chromium does not
    create any new window/contents but it has already notified
    us about which was the target URL. Then we can proceed to
    create a new session for that URL as usual in a new Tab.
    
    Fixes #1003

--- app/src/common/chromium/com/igalia/wolvic/browser/api/impl/TabWebContentsDelegate.java
+++ app/src/common/chromium/com/igalia/wolvic/browser/api/impl/TabWebContentsDelegate.java
@@ -5,14 +5,16 @@ import androidx.annotation.Nullable;
 
 import com.igalia.wolvic.browser.api.WSession;
 
+import org.chromium.base.task.PostTask;
+import org.chromium.base.task.TaskTraits;
 import org.chromium.blink.mojom.DisplayMode;
-import org.chromium.components.embedder_support.delegate.WebContentsDelegateAndroid;
 import org.chromium.content_public.browser.InvalidateTypes;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.url.GURL;
+import org.chromium.wolvic.WolvicWebContentsDelegate;
 
-public class TabWebContentsDelegate extends WebContentsDelegateAndroid {
+public class TabWebContentsDelegate extends WolvicWebContentsDelegate {
     private @NonNull SessionImpl mSession;
     private @NonNull final WebContents mWebContents;
 
@@ -69,6 +71,35 @@ public class TabWebContentsDelegate extends WebContentsDelegateAndroid {
                 delegate.onTitleChange(mSession, mWebContents.getTitle());
             }
         }
+    }
+
+    public class OnNewSessionCallback implements WSession.NavigationDelegate.OnNewSessionCallback {
+        public OnNewSessionCallback(RuntimeImpl runtime, GURL url) {
+            mRuntime = runtime;
+            mURL = url;
+        }
+
+        @Override
+        public void onNewSession(WSession session) {
+            ((SessionImpl) session).invokeOnReady(mRuntime, mURL.getSpec());
+        }
+
+        private RuntimeImpl mRuntime;
+        private GURL mURL;
+    }
+
+    @Override
+    public void onCreateNewWindow(GURL url) {
+        WSession.NavigationDelegate delegate = mSession.getNavigationDelegate();
+        assert delegate != null;
+
+        // We must return before executing this, otherwise we might end up messing around the native
+        // objects that chromium uses to back up some of the Java objects we use. For example the
+        // WebContentsDelegate created by Chromium might be freed by the onNewSession() call when it
+        // sets the new WebContentsDelegate object created by Wolvic.
+        PostTask.postDelayedTask(TaskTraits.UI_DEFAULT, () -> {
+            delegate.onNewSession(mSession, url.getSpec(), new OnNewSessionCallback(mSession.mRuntime, url));
+        }, 0);
     }
 
     @Override
