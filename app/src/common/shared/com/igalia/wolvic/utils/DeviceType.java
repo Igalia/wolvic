299abd5089b433ae1123da01a2aef1a393f4a469
Sergio Villar Senin <svillar@igalia.com>
2023-11-27 19:22:21 +0100

    Add a new VisionGlass build flavor and initial implementation
    Vision Glass is a device from Huawei which consists on a see-passthrough
    glasses that can be attached to a phone via an USB-C cable
    https://consumer.huawei.com/cn/wearables/vision-glass/. They're
    able to act as a "projector" of the phone contents. They're also
    interesting for XR use cases because they have an IMU that is able
    to report orientation data.
    
    This PR implements the build support and a basic implementation.
    So far we can display content on the glasses. There is no input
    interaction yet (apart from touches on the phone screen that can be
    used to generate input for Wolvic).

--- app/src/common/shared/com/igalia/wolvic/utils/DeviceType.java
+++ app/src/common/shared/com/igalia/wolvic/utils/DeviceType.java
@@ -12,7 +12,7 @@ import com.igalia.wolvic.BuildConfig;
 
 public class DeviceType {
     // These values need to match those in Device.h
-    @IntDef(value = {Unknown, OculusGo, OculusQuest, ViveFocus, ViveFocusPlus, PicoNeo2, PicoG2, PicoNeo3, OculusQuest2, HVR3DoF, HVR6DoF, PicoXR, MetaQuestPro, LynxR1, LenovoA3, LenovoVRX, MagicLeap2, MetaQuest3})
+    @IntDef(value = {Unknown, OculusGo, OculusQuest, ViveFocus, ViveFocusPlus, PicoNeo2, PicoG2, PicoNeo3, OculusQuest2, HVR3DoF, HVR6DoF, PicoXR, MetaQuestPro, LynxR1, LenovoA3, LenovoVRX, MagicLeap2, MetaQuest3, VisionGlass})
     public @interface Type {}
     public static final int Unknown = 0;
     public static final int OculusGo = 1;
@@ -32,6 +32,7 @@ public class DeviceType {
     public static final int LenovoVRX = 16;
     public static final int MagicLeap2 = 17;
     public static final int MetaQuest3 = 18;
+    public static final int VisionGlass = 19;
 
     private static @Type int mType = Unknown;
     private static String mDeviceName = "Unknown Device";
