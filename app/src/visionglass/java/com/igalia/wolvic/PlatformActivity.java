0afb10032a065188607a8680272f775079a3f528
Felipe Erias <felipeerias@igalia.com>
2024-01-19 15:00:40 +0900

    Clean up launch of Vision Glass
    Structure the code so it responds to changes in connected devices,
    USB permissions, etc.
    
    Implement OnConnectionListener to get notified when the device is
    connected and disconnected.
    
    Receive notifications when the USB permission has been granted.
    
    Try to ensure that the state of the app remains consistent.
    
    Add debug logs.

--- app/src/visionglass/java/com/igalia/wolvic/PlatformActivity.java
+++ app/src/visionglass/java/com/igalia/wolvic/PlatformActivity.java
@@ -5,25 +5,21 @@
 
 package com.igalia.wolvic;
 
-import androidx.activity.ComponentActivity;
-
-import androidx.annotation.Keep;
-
+import android.annotation.SuppressLint;
 import android.app.Presentation;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.hardware.display.DisplayManager;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.hardware.usb.UsbDevice;
+import android.hardware.display.DisplayManager;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.KeyEvent;
@@ -32,26 +28,34 @@ import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Lifecycle;
 
 import com.huawei.usblib.DisplayMode;
 import com.huawei.usblib.DisplayModeCallback;
+import com.huawei.usblib.OnConnectionListener;
 import com.huawei.usblib.VisionGlass;
 import com.igalia.wolvic.ui.widgets.Windows;
 import com.igalia.wolvic.utils.SystemUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public class PlatformActivity extends ComponentActivity implements SensorEventListener {
+public class PlatformActivity extends ComponentActivity implements SensorEventListener, OnConnectionListener {
     static String LOGTAG = SystemUtils.createLogtag(PlatformActivity.class);
+
+    public static final String HUAWEI_USB_PERMISSION = "com.huawei.usblib.USB_PERMISSION";
+
+    private boolean mWasImuStarted;
+    private boolean mIsAskingForPermission;
     private DisplayManager mDisplayManager;
     private Display mPresentationDisplay;
     private VisionGlassPresentation mActivePresentation;
@@ -95,7 +99,6 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
     };
     private final Runnable activityResumedRunnable = this::activityResumed;
 
-
     private interface PhoneUIButtonsCallback {
         void onButtonClicked(VRBrowserActivity activity);
     };
@@ -113,53 +116,92 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         callback.onButtonClicked((VRBrowserActivity)((VRBrowserApplication)context).getCurrentActivity());
     }
 
+    private final BroadcastReceiver mUsbPermissionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOGTAG, "USB permission broadcast. IMU started: " + mWasImuStarted +
+                    "; waiting for permission: " + mIsAskingForPermission + "; intent: " + intent.toString());
+
+            mIsAskingForPermission = false;
+            initVisionGlass();
+        }
+    };
+
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LOGTAG, "PlatformActivity onCreate");
         super.onCreate(savedInstanceState);
 
-        VisionGlass.getInstance().init(getApplication());
-
         mDisplayManager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
-        boolean wasImuStarted = false;
-        boolean isAskingForPermission = false;
-        do {
-            if (VisionGlass.getInstance().isConnected()) {
-                if (VisionGlass.getInstance().hasUsbPermission()) {
-                    Log.d(LOGTAG, "Device has USB permission -> registering callback for startImu");
-                    wasImuStarted = true;
-                    VisionGlass.getInstance().startImu((w, x, y, z) -> queueRunnable(() -> setHead(x, y, z, w)));
-                } else {
-                    if (isAskingForPermission) {
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        Log.w(LOGTAG, "Device does not have USB permission -> asking");
-                        VisionGlass.getInstance().requestUsbPermission();
-                        isAskingForPermission = true;
-                    }
-                }
-            } else {
-                // TODO: show a dialog asking the user to put on the glasses
-                Log.e(LOGTAG, "Device not connected" + (VisionGlass.getInstance().hasUsbPermission() ? " and does NOT have USB permissions" : ""));
-            }
-        } while (!wasImuStarted);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        // Alternatively: android.hardware.usb.action.USB_DEVICE_ATTACHED, USB_DEVICE_DETACHED.
+        IntentFilter usbPermissionFilter = new IntentFilter();
+        usbPermissionFilter.addAction(HUAWEI_USB_PERMISSION);
+        registerReceiver(mUsbPermissionReceiver, usbPermissionFilter);
+
+        VisionGlass.getInstance().init(getApplication());
+        VisionGlass.getInstance().setOnConnectionListener(this);
+        initVisionGlass();
+    }
+
+    @Override
+    public void onConnectionChange(boolean b) {
+        if (VisionGlass.getInstance().isConnected()) {
+            Log.d(LOGTAG, "onConnectionChange: Device connected");
+            initVisionGlass();
+        } else {
+            Log.d(LOGTAG, "onConnectionChange: Device disconnected");
+            if (mWasImuStarted) {
+                Log.d(LOGTAG, "onConnectionChange: Finish activity");
+                finish();
+            }
+        }
+    }
+
+    private void initVisionGlass() {
+        Log.d(LOGTAG, "initVisionGlass");
+
+        if (mWasImuStarted) {
+            Log.d(LOGTAG, "Duplicated call to init the Vision Glass system");
+            updateDisplays();
+            return;
+        }
+
+        if (!VisionGlass.getInstance().isConnected()) {
+            Log.d(LOGTAG, "Glasses not connected yet");
+            return;
+        }
+
+        if (!VisionGlass.getInstance().hasUsbPermission()) {
+            if (!mIsAskingForPermission) {
+                Log.d(LOGTAG, "Asking for USB permission");
+                mIsAskingForPermission = true;
+                VisionGlass.getInstance().requestUsbPermission();
+            }
+            return;
+        }
+
+        Log.d(LOGTAG, "Starting IMU");
+
+        mWasImuStarted = true;
+        VisionGlass.getInstance().startImu((w, x, y, z) -> queueRunnable(() -> setHead(x, y, z, w)));
 
         VisionGlass.getInstance().setDisplayMode(DisplayMode.vr3d, new DisplayModeCallback() {
             @Override
-            public void onSuccess(DisplayMode displayMode) { Log.d(LOGTAG, "Successfully switched to 3D mode"); }
+            public void onSuccess(DisplayMode displayMode) {
+                Log.d(LOGTAG, "Successfully switched to 3D mode");
+                updateDisplays();
+            }
 
             @Override
             public void onError(String s, int i) {
-                Log.d(LOGTAG, "Error " + i + " failed to switch to 3D mode " + s);
+                Log.d(LOGTAG, "Error " + i + "; failed to switch to 3D mode: " + s);
+                mWasImuStarted = false;
+                updateDisplays();
             }
         });
-
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         setContentView(R.layout.visionglass_layout);
 
@@ -208,6 +250,11 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         }));
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Show the app
+        if (getLifecycle().getCurrentState() == Lifecycle.State.RESUMED) {
+            updateDisplays();
+        }
     }
 
     // SensorEventListener overrides
@@ -250,12 +297,16 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
     protected void onPause() {
         Log.d(LOGTAG, "PlatformActivity onPause");
         super.onPause();
-        synchronized (mRenderLock) {
-            queueRunnable(activityPausedRunnable);
-            try {
-                mRenderLock.wait();
-            } catch(InterruptedException e) {
-                Log.e(LOGTAG, "activityPausedRunnable interrupted: " + e.toString());
+
+        // This check is needed to prevent a crash when pausing before 3D mode has started.
+        if (mWasImuStarted) {
+            synchronized (mRenderLock) {
+                queueRunnable(activityPausedRunnable);
+                try {
+                    mRenderLock.wait();
+                } catch(InterruptedException e) {
+                    Log.e(LOGTAG, "activityPausedRunnable interrupted: " + e);
+                }
             }
         }
         if (mActivePresentation != null)
@@ -271,30 +322,33 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         Log.d(LOGTAG, "PlatformActivity onResume");
         super.onResume();
 
-        updateDisplays();
-        showPresentation();
+        // Register to receive events from the display manager.
+        mDisplayManager.registerDisplayListener(mDisplayListener, null);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
+
+        if (VisionGlass.getInstance().isConnected() && VisionGlass.getInstance().hasUsbPermission()) {
+            updateDisplays();
+        }
 
         if (mActivePresentation != null && mActivePresentation.mGLView != null)
             mActivePresentation.mGLView.onResume();
 
         queueRunnable(activityResumedRunnable);
         setImmersiveSticky();
-
-        // Register to receive events from the display manager.
-        mDisplayManager.registerDisplayListener(mDisplayListener, null);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onDestroy() {
         Log.d(LOGTAG, "PlatformActivity onDestroy");
         super.onDestroy();
+        unregisterReceiver(mUsbPermissionReceiver);
         synchronized (mRenderLock) {
-            queueRunnable(activityDestroyedRunnable);
-            try {
-                mRenderLock.wait();
-            } catch(InterruptedException e) {
-                Log.e(LOGTAG, "activityDestroyedRunnable interrupted: " + e.toString());
+            if (queueRunnable(activityDestroyedRunnable)) {
+                try {
+                    mRenderLock.wait();
+                } catch (InterruptedException e) {
+                    Log.e(LOGTAG, "activityDestroyedRunnable interrupted: " + e.toString());
+                }
             }
         }
     }
@@ -320,17 +374,20 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         }
     }
 
-    void queueRunnable(Runnable aRunnable) {
+    boolean queueRunnable(@NonNull Runnable aRunnable) {
         if (mActivePresentation != null) {
             mActivePresentation.mGLView.queueEvent(aRunnable);
+            return true;
         } else {
             synchronized (mPendingEvents) {
                 mPendingEvents.add(aRunnable);
             }
             if (mActivePresentation != null) {
                 notifyPendingEvents();
+                return true;
             }
         }
+        return false;
     }
 
     private void notifyPendingEvents() {
@@ -346,26 +403,39 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         Display[] displays = mDisplayManager.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
         if (displays.length == 0) {
             mPresentationDisplay = null;
+            Log.d(LOGTAG, "updateDisplays: could not find a Presentation display");
             return;
         }
 
+        Log.d(LOGTAG, "updateDisplays: found Presentation display");
+        if (mPresentationDisplay != displays[0]) {
+            if (mActivePresentation != null) {
+                mActivePresentation.mGLView.onPause();
+            }
+            mActivePresentation = null;
+        }
         mPresentationDisplay = displays[0];
+
+        runOnUiThread(this::showPresentation);
     }
 
     private final DisplayManager.DisplayListener mDisplayListener =
             new DisplayManager.DisplayListener() {
                 @Override
                 public void onDisplayAdded(int displayId) {
+                    Log.d(LOGTAG, "display listener: onDisplayAdded displayId = " + displayId);
                     updateDisplays();
                 }
 
                 @Override
                 public void onDisplayChanged(int displayId) {
+                    Log.d(LOGTAG, "display listener: onDisplayChanged displayId = " + displayId);
                     updateDisplays();
                 }
 
                 @Override
                 public void onDisplayRemoved(int displayId) {
+                    Log.d(LOGTAG, "display listener: onDisplayRemoved displayId = " + displayId);
                     updateDisplays();
                 }
             };
@@ -388,6 +458,7 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         }
         VisionGlassPresentation presentation = new VisionGlassPresentation(this, mPresentationDisplay);
         Display.Mode [] modes = mPresentationDisplay.getSupportedModes();
+        Log.d(LOGTAG, "showPresentation supported modes: " + Arrays.toString(modes));
         presentation.setPreferredDisplayMode(modes[0].getModeId());
         presentation.show();
         presentation.setOnDismissListener(mOnDismissListener);
@@ -406,6 +477,7 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
          * Sets the preferred display mode id for the presentation.
          */
         public void setPreferredDisplayMode(int modeId) {
+            Log.d(LOGTAG, "VisionGlassPresentation setPreferredDisplayMode: " + modeId);
             WindowManager.LayoutParams params = getWindow().getAttributes();
             params.preferredDisplayModeId = modeId;
             getWindow().setAttributes(params);
@@ -415,10 +487,7 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
         protected void onCreate(Bundle savedInstanceState) {
             // Be sure to call the super class.
             super.onCreate(savedInstanceState);
-
-            // Get the resources for the context of the presentation.
-            // Notice that we are getting the resources from the context of the presentation.
-            Resources r = getContext().getResources();
+            Log.d(LOGTAG, "VisionGlassPresentation onCreate");
 
             // Inflate the layout.
             setContentView(R.layout.visionglass_presentation_layout);
@@ -431,12 +500,14 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
             mGLView.setRenderer(new GLSurfaceView.Renderer() {
                 @Override
                 public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+                    Log.d(LOGTAG, "VisionGlassPresentation onSurfaceCreated");
                     activityCreated(getAssets());
                     notifyPendingEvents();
                 }
 
                 @Override
                 public void onSurfaceChanged(GL10 gl, int width, int height) {
+                    Log.d(LOGTAG, "VisionGlassPresentation onSurfaceChanged");
                     updateViewport(width, height);
                 }
 
@@ -451,7 +522,7 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
     @Keep
     @SuppressWarnings("unused")
     private void setRenderMode(final int aMode) {
-        runOnUiThread(() -> setImmersiveSticky());
+        runOnUiThread(this::setImmersiveSticky);
     }
 
     private native void activityCreated(Object aAssetManager);
