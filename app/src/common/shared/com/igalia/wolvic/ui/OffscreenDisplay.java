4636fe1ead13186f08ea0f0c233cb7ee06dbdaef
Ha Anh Vu <75315486+haanhvu@users.noreply.github.com>
2024-02-05 23:49:17 +0700

    Modernize deprecated getMetrics(DisplayMetrics) in Display (#1223)
    Replaced it by WindowMetrics API whenever available.

--- app/src/common/shared/com/igalia/wolvic/ui/OffscreenDisplay.java
+++ app/src/common/shared/com/igalia/wolvic/ui/OffscreenDisplay.java
@@ -7,6 +7,7 @@ package com.igalia.wolvic.ui;
 
 import android.app.Presentation;
 import android.content.Context;
+import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
@@ -20,6 +21,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.view.WindowMetrics;
 
 import com.igalia.wolvic.VRBrowserActivity;
 import com.igalia.wolvic.utils.SystemUtils;
@@ -88,8 +91,16 @@ public class OffscreenDisplay {
             Display defaultDisplay = manager.getDisplay(Display.DEFAULT_DISPLAY);
 
             int flags = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY;
-            // TODO: Deprecated getMetrics(DisplayMetrics), see https://github.com/Igalia/wolvic/issues/799
-            defaultDisplay.getMetrics(mDefaultMetrics);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+                WindowMetrics metrics = windowManager.getCurrentWindowMetrics();
+                Rect bounds = metrics.getBounds();
+                mDefaultMetrics.widthPixels = bounds.width();
+                mDefaultMetrics.heightPixels = bounds.height();
+                mDefaultMetrics.density = metrics.getDensity();
+            } else {
+                defaultDisplay.getMetrics(mDefaultMetrics);
+            }
 
             mVirtualDisplay = manager.createVirtualDisplay("OffscreenViews Overlay", mWidth, mHeight,
                     mDefaultMetrics.densityDpi, mSurface, flags);
