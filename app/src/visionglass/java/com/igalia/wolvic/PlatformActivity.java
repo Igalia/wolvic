18e8e80c695da4f9765974ec5faefacccd10cb50
Felipe Erias <felipeerias@igalia.com>
2024-02-28 14:01:10 +0900

    Don't force the Vision Glass app to be fullscreen
    Remove codesetImmersiveSticky

--- app/src/visionglass/java/com/igalia/wolvic/PlatformActivity.java
+++ app/src/visionglass/java/com/igalia/wolvic/PlatformActivity.java
@@ -330,7 +330,6 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
             mActivePresentation.mGLView.onResume();
 
         queueRunnable(activityResumedRunnable);
-        setImmersiveSticky();
     }
 
     @Override
@@ -346,27 +345,6 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
                     Log.e(LOGTAG, "activityDestroyedRunnable interrupted: " + e.toString());
                 }
             }
-        }
-    }
-
-    void setImmersiveSticky() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            getWindow().setDecorFitsSystemWindows(false);
-            WindowInsetsController controller = getWindow().getInsetsController();
-            if (controller != null) {
-                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-            }
-        } else {
-            getWindow()
-                    .getDecorView()
-                    .setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
 
@@ -644,9 +622,7 @@ public class PlatformActivity extends ComponentActivity implements SensorEventLi
 
     @Keep
     @SuppressWarnings("unused")
-    private void setRenderMode(final int aMode) {
-        runOnUiThread(this::setImmersiveSticky);
-    }
+    private void setRenderMode(final int aMode) {}
 
     private native void activityCreated(Object aAssetManager);
     private native void updateViewport(int width, int height);
