0635b45458b1bfb2e68712b23f30a5f54b0a4fad
Sergio Villar Senin <svillar@igalia.com>
2024-04-04 17:13:46 +0200

    [VisionGlass] Add scroll gesture to the phone touchpad
    From now on users can scroll web pages by scrolling on the touchpad
    of the phone UI. This involved a set of changes:
    
    1. Migrate from onTouchEvent() to GestureManager.onTouchEvent().
    Instead of handling the low level UP/DOWN events the code will
    deal with onScroll, onSingleTap events recognized by the
    GestureManager. Due to this change scrolling via tap and tilt
    the controller (phone) is no longer possible.
    2. Convert PlatformActivityPlugin into an abstract class. This
    was done to allow some method implementations. We added a new
    interface implemented by VRBrowserActivity to get scroll
    notifications from the platform plugins.
    3. Use the current event generation machinery to deliver
    a scroll event to the currently hovered widget. This is generally
    the web widget but it could be another one like the settings
    dialog.

--- app/src/common/shared/com/igalia/wolvic/PlatformActivityPlugin.java
+++ app/src/common/shared/com/igalia/wolvic/PlatformActivityPlugin.java
@@ -1,6 +1,25 @@
 package com.igalia.wolvic;
 
-public interface PlatformActivityPlugin {
-    void onKeyboardVisibilityChange(boolean isVisible);
-    void onVideoAvailabilityChange();
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PlatformActivityPlugin {
+    public interface PlatformActivityPluginListener {
+        void onPlatformScrollEvent(float distanceX, float distanceY);
+    }
+    abstract void onKeyboardVisibilityChange(boolean isVisible);
+    abstract void onVideoAvailabilityChange();
+    void registerListener(PlatformActivityPluginListener listener) {
+        if (mListeners == null)
+            mListeners = new ArrayList<>();
+        mListeners.add(listener);
+    }
+    void unregisterListener(PlatformActivityPluginListener listener) {
+        mListeners.remove(listener);
+    }
+    void notifyOnScrollEvent(float distanceX, float distanceY) {
+        for (PlatformActivityPluginListener listener : mListeners)
+            listener.onPlatformScrollEvent(distanceX, distanceY);
+    }
+    private List<PlatformActivityPluginListener> mListeners;
 }
