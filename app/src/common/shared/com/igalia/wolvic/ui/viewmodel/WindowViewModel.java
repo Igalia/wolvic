Igalia/wolvic

commit 136950b008525a737f2b999d9c102ae444e70f94

Initial implementation of Kiosk mode (#234)
This is the initial implementation of Kiosk mode, which allows to launch Wolvic in a configuration that will disable most of the UI and only display one website.

To try it out, run the following command on the terminal for HVR:

adb shell am start -a com.huawei.android.vr.action.MAIN -d "https://moonrider.xyz" com.igalia.wolvic/com.igalia.wolvic.VRBrowserActivity

app/src/common/shared/com/igalia/wolvic/ui/viewmodel/WindowViewModel.java
@@ -37,6 +37,7 @@ public class WindowViewModel extends AndroidViewModel {
     private MutableLiveData<Windows.WindowPlacement> placement;
     private MutableLiveData<ObservableBoolean> isOnlyWindow;
     private MutableLiveData<ObservableBoolean> isFullscreen;
+    private MutableLiveData<ObservableBoolean> isKioskMode;
     private MediatorLiveData<ObservableBoolean> isTopBarVisible;
     private MutableLiveData<ObservableBoolean> isResizeMode;
     private MutableLiveData<ObservableBoolean> isPrivateSession;
@@ -86,12 +87,14 @@ public class WindowViewModel extends AndroidViewModel {
         placement = new MutableLiveData<>(Windows.WindowPlacement.FRONT);
         isOnlyWindow = new MutableLiveData<>(new ObservableBoolean(false));
         isFullscreen = new MutableLiveData<>(new ObservableBoolean(false));
+        isKioskMode = new MutableLiveData<>(new ObservableBoolean(false));
         isResizeMode = new MutableLiveData<>(new ObservableBoolean(false));
         isPrivateSession = new MutableLiveData<>(new ObservableBoolean(false));
 
         isTopBarVisible = new MediatorLiveData<>();
         isTopBarVisible.addSource(isOnlyWindow, mIsTopBarVisibleObserver);
         isTopBarVisible.addSource(isFullscreen, mIsTopBarVisibleObserver);
+        isTopBarVisible.addSource(isKioskMode, mIsTopBarVisibleObserver);
         isTopBarVisible.addSource(isResizeMode, mIsTopBarVisibleObserver);
         isTopBarVisible.addSource(isPrivateSession, mIsTopBarVisibleObserver);
         isTopBarVisible.addSource(isWindowVisible, mIsTopBarVisibleObserver);
@@ -102,6 +105,7 @@ public class WindowViewModel extends AndroidViewModel {
         showClearButton.addSource(isPrivateSession, mShowClearButtonObserver);
         showClearButton.addSource(isResizeMode, mShowClearButtonObserver);
         showClearButton.addSource(isFullscreen, mShowClearButtonObserver);
+        showClearButton.addSource(isKioskMode, mShowClearButtonObserver);
         showClearButton.addSource(isWindowVisible, mShowClearButtonObserver);
         showClearButton.setValue(new ObservableBoolean(false));
 
@@ -110,6 +114,7 @@ public class WindowViewModel extends AndroidViewModel {
 
         isTitleBarVisible = new MediatorLiveData<>();
         isTitleBarVisible.addSource(isFullscreen, mIsTitleBarVisibleObserver);
+        isTitleBarVisible.addSource(isKioskMode, mIsTitleBarVisibleObserver);
         isTitleBarVisible.addSource(isResizeMode, mIsTitleBarVisibleObserver);
         isTitleBarVisible.addSource(isActiveWindow, mIsTitleBarVisibleObserver);
         isTitleBarVisible.addSource(isWindowVisible, mIsTitleBarVisibleObserver);
@@ -174,7 +179,7 @@ public class WindowViewModel extends AndroidViewModel {
     private Observer<ObservableBoolean> mIsTopBarVisibleObserver = new Observer<ObservableBoolean>() {
         @Override
         public void onChanged(ObservableBoolean o) {
-            if (isFullscreen.getValue().get() || isResizeMode.getValue().get() || !isWindowVisible.getValue().get()) {
+            if (isFullscreen.getValue().get() || isKioskMode.getValue().get() || isResizeMode.getValue().get() || !isWindowVisible.getValue().get()) {
                 isTopBarVisible.postValue(new ObservableBoolean(false));
 
             } else {
@@ -193,14 +198,15 @@ public class WindowViewModel extends AndroidViewModel {
         public void onChanged(ObservableBoolean o) {
             showClearButton.postValue(new ObservableBoolean(isWindowVisible.getValue().get() &&
                     isPrivateSession.getValue().get() && isOnlyWindow.getValue().get() &&
-                    !isResizeMode.getValue().get() && !isFullscreen.getValue().get()));
+                    !isResizeMode.getValue().get() && !isFullscreen.getValue().get() &&
+                    !isKioskMode.getValue().get()));
         }
     };
 
     private Observer<ObservableBoolean> mIsTitleBarVisibleObserver = new Observer<ObservableBoolean>() {
         @Override
         public void onChanged(ObservableBoolean o) {
-            if (isFullscreen.getValue().get() || isResizeMode.getValue().get() || isActiveWindow.getValue().get()) {
+            if (isFullscreen.getValue().get() || !isKioskMode.getValue().get() || isResizeMode.getValue().get() || isActiveWindow.getValue().get()) {
                 isTitleBarVisible.postValue(new ObservableBoolean(false));
 
             } else {
@@ -453,6 +459,15 @@ public class WindowViewModel extends AndroidViewModel {
 
     public void setIsFullscreen(boolean isFullscreen) {
         this.isFullscreen.postValue(new ObservableBoolean(isFullscreen));
+    }
+
+    @NonNull
+    public MutableLiveData<ObservableBoolean> getIsKioskMode() {
+        return isKioskMode;
+    }
+
+    public void setIsKioskMode(boolean isKioskMode) {
+        this.isKioskMode.postValue(new ObservableBoolean(isKioskMode));
     }
 
     @NonNull

app/src/common/shared/com/igalia/wolvic/VRBrowserActivity.java
@@ -99,6 +99,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
@@ -471,6 +472,9 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
         if (SettingsStore.getInstance(this).isWhatsNewDisplayed()) {
             return;
         }
+        if (shouldOpenInKioskMode(getIntent())) {
+            return;
+        }
         mWhatsNewWidget = new WhatsNewWidget(this);
         mWhatsNewWidget.setLoginOrigin(Accounts.LoginOrigin.NONE);
         mWhatsNewWidget.getPlacement().parentHandle = mWindows.getFocusedWindow().getHandle();
@@ -745,15 +749,30 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
 
             int location = Windows.OPEN_IN_FOREGROUND;
 
-            if (openInWindow) {
-                location = Windows.OPEN_IN_NEW_WINDOW;
-            } else if (openInBackground) {
-                location = Windows.OPEN_IN_BACKGROUND;
+            if (shouldOpenInKioskMode(intent)) {
+                // FIXME this might not work as expected if the app was already running
+                mWindows.openInKioskMode(uri.toString());
+            } else {
+                if (openInWindow) {
+                    location = Windows.OPEN_IN_NEW_WINDOW;
+                } else if (openInBackground) {
+                    location = Windows.OPEN_IN_BACKGROUND;
+                }
+                mWindows.openNewTabAfterRestore(uri.toString(), location);
             }
-            mWindows.openNewTabAfterRestore(uri.toString(), location);
         } else {
             mWindows.getFocusedWindow().loadHomeIfBlank();
         }
+    }
+
+    private boolean shouldOpenInKioskMode(@NonNull Intent intent) {
+        if (DeviceType.isHVRBuild()) {
+            // This action is specific to HVR in Virtual Reality mode.
+            // The data of the Intent points at the URL that will be opened in kiosk mode.
+            return Objects.equals(intent.getAction(), "com.huawei.android.vr.action.MAIN")
+                    && intent.getData() != null;
+        }
+        return false;
     }
 
     private ConnectivityReceiver.Delegate mConnectivityDelegate = connected -> {

app/src/common/shared/com/igalia/wolvic/browser/engine/Session.java
@@ -1764,6 +1764,7 @@ public class Session implements WContentBlocking.Delegate, WSession.NavigationDe
         Log.d(LOGTAG, "\tActive: " + mState.isActive());
         Log.d(LOGTAG, "\tUri: " + (mState.mUri != null ? mState.mUri : "null"));
         Log.d(LOGTAG, "\tFullscreen: " + mState.mFullScreen);
+        Log.d(LOGTAG, "\tKiosk mode: " + mState.mInKioskMode);
         Log.d(LOGTAG, "\tCan go back: " + mState.mCanGoBack);
         Log.d(LOGTAG, "\tCan go forward: " + mState.mCanGoForward);
         if (mState.mSettings != null) {

app/src/common/shared/com/igalia/wolvic/browser/engine/SessionState.java
@@ -49,6 +49,7 @@ public class SessionState {
     public String mPreviousUri;
     public String mTitle = "";
     public transient boolean mFullScreen;
+    public transient boolean mInKioskMode;
     public transient WSession mSession;
     public transient WDisplay mDisplay;
     public SessionSettings mSettings;

app/src/common/shared/com/igalia/wolvic/ui/widgets/NavigationBarWidget.java
@@ -643,6 +643,17 @@ public class NavigationBarWidget extends UIWidget implements WSession.Navigation
     }
 
     @Override
+    public void onKioskMode(WindowWidget aWindow, boolean isKioskMode) {
+        if (isKioskMode) {
+            mTrayViewModel.setShouldBeVisible(false);
+            hide(KEEP_WIDGET);
+        } else {
+            mTrayViewModel.setShouldBeVisible(true);
+            show(KEEP_FOCUS);
+        }
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
     }

app/src/common/shared/com/igalia/wolvic/ui/widgets/WindowWidget.java
@@ -150,6 +150,7 @@ public class WindowWidget extends UIWidget implements SessionChangeListener,
         default void onFullScreen(@NonNull WindowWidget aWindow, boolean aFullScreen) {}
         default void onMediaFullScreen(@NonNull final WMediaSession mediaSession, boolean aFullScreen) {}
         default void onVideoAvailabilityChanged(@NonNull WindowWidget aWindow) {}
+        default void onKioskMode(WindowWidget aWindow, boolean isKioskMode) {}
     }
 
     @Override
@@ -932,6 +933,20 @@ public class WindowWidget extends UIWidget implements SessionChangeListener,
 
     public boolean isFullScreen() {
         return mViewModel.getIsFullscreen().getValue().get();
+    }
+
+    public void setKioskMode(boolean isKioskMode) {
+        if (mViewModel.getIsKioskMode().getValue().get() == isKioskMode)
+            return;
+
+        mViewModel.setIsKioskMode(isKioskMode);
+        for (WindowListener listener : mListeners) {
+            listener.onKioskMode(this, isKioskMode);
+        }
+    }
+
+    public boolean isKioskMode() {
+        return mViewModel.getIsKioskMode().getValue().get();
     }
 
     public void addWindowListener(WindowListener aListener) {

app/src/common/shared/com/igalia/wolvic/ui/widgets/Windows.java
@@ -1280,6 +1280,11 @@ public class Windows implements TrayListener, TopBarWidget.Delegate, TitleBarWid
         }
     }
 
+    @Override
+    public void onKioskMode(WindowWidget aWindow, boolean isKioskMode) {
+        // TODO
+    }
+
 public void selectTab(@NonNull Session aTab) {
         onTabSelect(aTab);
     }
@@ -1358,6 +1363,14 @@ public void selectTab(@NonNull Session aTab) {
                 addBackgroundTab(mFocusedWindow, aUri);
             }
         }
+    }
+
+    public void openInKioskMode(@NonNull String aUri) {
+        Session session = SessionStore.get().createSuspendedSession(aUri, true);
+        setFirstPaint(mFocusedWindow, session);
+        mFocusedWindow.setSession(session, WindowWidget.DEACTIVATE_CURRENT_SESSION);
+        mFocusedWindow.setKioskMode(true);
+        mFocusedWindow.setIsFullScreen(true);
     }
 
     public void addTab(@NonNull WindowWidget targetWindow, @Nullable String aUri) {

commit 6e740391f3d839feccf01ef4d69d71ef1e63eadd

Bring back the title bar widget
Amazingly the title bar widget visibility has been broken for almost
two years without anyone realizing about it. I figured it out by chance
after doing some testing with the old FxR and then voilà the title bar
was there with the very cool play/pause media button.

The visibility was broken in 136950b0 when Kiosk mode was added to
Wolvic. The problem was that the visibility observer was returning
false whenever kiosk mode was disabled instead of doing it when
kiosk mode was enabled (as there are not multiple windows in kiosk).

app/src/common/shared/com/igalia/wolvic/ui/viewmodel/WindowViewModel.java
@@ -215,7 +215,7 @@ public class WindowViewModel extends AndroidViewModel {
     private Observer<ObservableBoolean> mIsTitleBarVisibleObserver = new Observer<ObservableBoolean>() {
         @Override
         public void onChanged(ObservableBoolean o) {
-            if (isFullscreen.getValue().get() || !isKioskMode.getValue().get() || isResizeMode.getValue().get() || isActiveWindow.getValue().get()) {
+            if (isFullscreen.getValue().get() || isKioskMode.getValue().get() || isResizeMode.getValue().get() || isActiveWindow.getValue().get()) {
                 isTitleBarVisible.postValue(new ObservableBoolean(false));
 
             } else {