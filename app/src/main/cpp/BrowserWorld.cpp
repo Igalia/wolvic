5353b4ead143a63fe3d0e604adac50eb84679a78
Felipe Erias <felipeerias@igalia.com>
2024-09-11 17:15:27 +0900

    [Oculus] Fix immersive environments
    To construct the immersive environment, Wolvic creates an OpenXR
    cubemap layer and applies textures to it.
    
    For Oculus, we were using compressed textures loaded from KTX files.
    
    Starting with version 69 of the Meta OS, this method stopped working
    and the environments just looked pitch black. It seems that the
    problem is caused by a bug on the system that breaks the use
    of compressed textures on a cubemap layer.
    
    While this bug is fixed upstream, this patch forces Oculus devices
    to load the immersive environments from PNG files.
    
    Fixes https://github.com/Igalia/wolvic/issues/1535

--- app/src/main/cpp/BrowserWorld.cpp
+++ app/src/main/cpp/BrowserWorld.cpp
@@ -1984,8 +1984,9 @@ BrowserWorld::CreateSkyBox(const std::string& aBasePath, const std::string& aExt
   const std::string extension = aExtension.empty() ? ".png" : aExtension;
   GLenum glFormat = GL_SRGB8_ALPHA8;
 #elif OCULUSVR
-  const std::string extension = aExtension.empty() ? ".ktx" : aExtension;
-  GLenum glFormat = extension == ".ktx" ? GL_COMPRESSED_SRGB8_ETC2 : GL_SRGB8_ALPHA8;
+  // Meta Quest (after v69) does not support compressed textures for the cubemap.
+  const std::string extension = aExtension.empty() ? ".png" : aExtension;
+  GLenum glFormat = GL_SRGB8_ALPHA8;
 #else
   const std::string extension = aExtension.empty() ? ".ktx" : aExtension;
   GLenum glFormat = extension == ".ktx" ? GL_COMPRESSED_RGB8_ETC2 : GL_RGBA8;
