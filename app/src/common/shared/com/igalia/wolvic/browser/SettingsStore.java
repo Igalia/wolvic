bbd00c4406137fc597cdf3a877d928a82010ae25
haanhvu <haanh6594@gmail.com>
2024-10-22 22:52:49 +0700

    Clean up

--- app/src/common/shared/com/igalia/wolvic/browser/SettingsStore.java
+++ app/src/common/shared/com/igalia/wolvic/browser/SettingsStore.java
@@ -105,8 +105,6 @@ public class SettingsStore {
     // The maximum size is computed so the resulting texture fits within 2560x2560.
     public final static int MAX_WINDOW_WIDTH_DEFAULT = 1200;
     public final static int MAX_WINDOW_HEIGHT_DEFAULT = 800;
-    public final static String[] WINDOW_SIZES = {"800x450", "800x600", "1000x562", "1000x750"};
-
     public enum WindowSizePreset {
         PRESET_0(800, 450),
         PRESET_1(800, 600),
@@ -121,7 +119,6 @@ public class SettingsStore {
             this.height = height;
         }
     }
-
     public static int windowSizeId;
 
     public final static int POINTER_COLOR_DEFAULT_DEFAULT = Color.parseColor("#FFFFFF");
@@ -505,12 +502,6 @@ public class SettingsStore {
     }
 
     public void setWindowSize(int id) {
-        /*String[] widthAndHeight = WINDOW_SIZES[id].split("x");
-        SharedPreferences.Editor editor = mPrefs.edit();
-        editor.putInt(mContext.getString(R.string.settings_key_window_width), Integer.parseInt(widthAndHeight[0]));
-        editor.putInt(mContext.getString(R.string.settings_key_window_height), Integer.parseInt(widthAndHeight[1]));
-        editor.commit();*/
-
         WindowSizePreset sizePreset = WindowSizePreset.values()[id];
         SharedPreferences.Editor editor = mPrefs.edit();
         editor.putInt(mContext.getString(R.string.settings_key_window_width), sizePreset.width);
