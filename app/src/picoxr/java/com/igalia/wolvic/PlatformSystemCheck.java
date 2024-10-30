2fb82bb7754cf2ddae56775f056b2c92408c4fe3
Sergio Villar Senin <svillar@igalia.com>
2024-10-30 19:14:18 +0100

    Revert "[Pico] Add a OS version check"
    This reverts commit 45d7a1dc91638a0e003a7677291f9ba8f113a74d.

--- app/src/picoxr/java/com/igalia/wolvic/PlatformSystemCheck.java
+++ app/src/picoxr/java/com/igalia/wolvic/PlatformSystemCheck.java
@@ -1,44 +1,10 @@
 package com.igalia.wolvic;
 
-import android.util.Log;
-
-import com.igalia.wolvic.utils.SystemUtils;
-
 public class PlatformSystemCheck extends SystemCheck {
-    private static final String PICO_OS_VERSION_PROPERTY = "ro.build.display.id";
-    private static final int MIN_PICO_OS_MAJOR_VERSION = 5;
-    private static final int MIN_PICO_OS_MINOR_VERSION = 7;
-    private static final int MIN_PICO_OS_PATCH_VERSION = 1;
-    private static final String LOGTAG = SystemUtils.createLogtag(PlatformSystemCheck.class);
-
-    /**
-     * Compares two version strings in the form x.y.z where x, y, and z are integers.
-     * @return a negative integer, zero, or a positive integer as the first version
-     *         is less than, equal to, or greater than the second version.
-     */
-    private int compare(String str1, String str2) {
-        String[] parts1 = str1.split("\\.");
-        String[] parts2 = str2.split("\\.");
-
-        for (int i = 0; i < Math.min(parts1.length, parts2.length); i++) {
-            int num1 = Integer.parseInt(parts1[i]);
-            int num2 = Integer.parseInt(parts2[i]);
-
-            if (num1 != num2)
-                return num1 - num2;
-        }
-        return parts1.length - parts2.length;
-    }
 
     @Override
-    public boolean isOSVersionCompatible() {
-        String osVersion = getSystemProperty(PICO_OS_VERSION_PROPERTY);
-        Log.i(LOGTAG, "Checking that OS version is at least " + minSupportedVersion() + " (found " + osVersion + ")");
-        return compare(osVersion, minSupportedVersion()) >= 0;
-    }
+    public boolean isOSVersionCompatible() { return true; }
 
     @Override
-    public String minSupportedVersion() {
-        return MIN_PICO_OS_MAJOR_VERSION + "." + MIN_PICO_OS_MINOR_VERSION + "." + MIN_PICO_OS_PATCH_VERSION;
-    }
+    public String minSupportedVersion() { return ""; }
 }
