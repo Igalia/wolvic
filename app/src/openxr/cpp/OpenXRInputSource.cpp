ba1e64c5229ad6a0afc8b0434760f54fc0359d0b
Sergio <svillar@igalia.com>
2025-01-15 14:08:11 +0100

    [Lynx] Workaround a runtime bug not reporting tracked hand positions (#1681)
    Lynx runtime never returns the XR_SPACE_LOCATION_POSITION_TRACKED_BIT
    flag as active, even if it's really actively tracking the position
    of the hands. That confused the code and made it think that emulated
    positions were returned instead.
    
    That was causing the origin of the aim pose (and thus the origin
    of the aim ray) to be incorrectly shifted from the hands. The
    workaround is replacing the check for POSITION_TRACKED_BIT by
    POSITION_VALID_BIT. That way we'd still be able to detected when
    hands are not tracked while providing a much better aim.

--- app/src/openxr/cpp/OpenXRInputSource.cpp
+++ app/src/openxr/cpp/OpenXRInputSource.cpp
@@ -873,7 +873,12 @@ void OpenXRInputSource::Update(const XrFrameState& frameState, XrSpace localSpac
     delegate.SetHandActionEnabled(mIndex, isHandActionEnabled);
 
     device::CapabilityFlags flags = device::Orientation;
+#ifdef LYNX
+    // Lynx runtime incorrectly never sets the TRACKED bit.
+    const bool positionTracked = poseLocation.locationFlags & XR_SPACE_LOCATION_POSITION_VALID_BIT;
+#else
     const bool positionTracked = poseLocation.locationFlags & XR_SPACE_LOCATION_POSITION_TRACKED_BIT;
+#endif
     flags |= positionTracked ? device::Position : device::PositionEmulated;
 
     vrb::Matrix pointerTransform = XrPoseToMatrix(poseLocation.pose);
