fad93b6ed2587bb060e76d40cc83c3432351bd6f
Sergio Villar Senin <svillar@igalia.com>
2025-02-18 17:48:07 +0100

    Remove glean leftovers
    We do not use glean for telemetry anymore. That's why we removed it
    a while ago but there were still some leftovers that need to be
    removed.

--- app/src/test/java/com/igalia/wolvic/EnvironmentsTest.kt
+++ app/src/test/java/com/igalia/wolvic/EnvironmentsTest.kt
@@ -5,7 +5,6 @@ import androidx.test.core.app.ApplicationProvider
 import com.igalia.wolvic.browser.SettingsStore
 import com.igalia.wolvic.utils.EnvironmentUtils
 import com.igalia.wolvic.utils.TestFileUtils
-import mozilla.components.service.glean.testing.GleanTestRule
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
