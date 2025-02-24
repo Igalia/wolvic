906f84510f5807811b5f07e33292ebc780c3c40d
Sergio Villar Senin <svillar@igalia.com>
2025-02-24 17:09:09 +0100

    Refactor AndroidMediaPlayer to use raw resources
    Instead of storing the audio files assets we will store them now
    as raw resources. This will help us moving towards implementing the
    audio media player to SoundPool.
    
    We'd still have to keep the current implementation for Meta devices
    that do not deal well with SoundPool.

--- app/src/common/shared/com/igalia/wolvic/audio/VRAudioTheme.java
+++ app/src/common/shared/com/igalia/wolvic/audio/VRAudioTheme.java
@@ -5,25 +5,18 @@
 
 package com.igalia.wolvic.audio;
 
+import com.igalia.wolvic.R;
+
 public class VRAudioTheme implements AudioEngine.AudioTheme {
     @Override
-    public String getPath(AudioEngine.Sound aSound) {
-        String name = filename(aSound);
-        if (name != null) {
-            return "sounds/" + name;
-        }
-        return null;
-    }
-
-    private String filename(AudioEngine.Sound aSound) {
+    public int getResourceId(AudioEngine.Sound aSound) {
         switch (aSound) {
-            case KEYBOARD: return "keyboard_sound.wav";
-            case CLICK: return "click.wav";
-            case BACK: return "back.wav";
-            case EXIT: return "exit.wav";
-            case ERROR: return "error.wav";
+            case KEYBOARD: return R.raw.keyboard_sound;
+            case CLICK: return R.raw.click_sound;
+            case BACK: return R.raw.back_sound;
+            case EXIT: return R.raw.exit_sound;
+            case ERROR: return R.raw.error_sound;
         }
-
-        return null;
+        return 0;
     }
 }
