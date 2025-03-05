0710e567734a567c7b3cac20e1ba6b1351b89b88
Felipe Erias <felipeerias@igalia.com>
2025-03-05 13:59:16 +0900

    Top Sites in New Tab
    Add the Top Sites component, which provides a list of frequent and
    recent sites, as well as default and pinned ones.
    
    The default sites are empty for now, but we can add a few of them,
    including ones related to Wolvic.

--- app/src/common/shared/com/igalia/wolvic/ui/views/NewTabView.java
+++ app/src/common/shared/com/igalia/wolvic/ui/views/NewTabView.java
@@ -11,11 +11,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.VRBrowserActivity;
+import com.igalia.wolvic.browser.components.TopSitesAdapter;
+import com.igalia.wolvic.browser.components.TopSitesHelper;
 import com.igalia.wolvic.browser.engine.Session;
 import com.igalia.wolvic.browser.engine.SessionStore;
 import com.igalia.wolvic.databinding.NewTabBinding;
+import com.igalia.wolvic.ui.adapters.TopSitesAdapterImpl;
 import com.igalia.wolvic.ui.viewmodel.SettingsViewModel;
 import com.igalia.wolvic.utils.SystemUtils;
+
+import mozilla.components.feature.top.sites.TopSite;
+import mozilla.components.feature.top.sites.TopSitesFeature;
 
 public class NewTabView extends FrameLayout {
 
@@ -23,6 +29,8 @@ public class NewTabView extends FrameLayout {
 
     private NewTabBinding mBinding;
     private SettingsViewModel mSettingsViewModel;
+    private TopSitesAdapterImpl mTopSitesAdapter;
+    private TopSitesFeature mTopSitesFeature;
 
     public NewTabView(Context context) {
         super(context);
@@ -46,10 +54,49 @@ public class NewTabView extends FrameLayout {
         mBinding.setSettingsmodel(mSettingsViewModel);
 
         mBinding.logo.setOnClickListener(v -> openUrl(getContext().getString(R.string.home_page_url)));
+
+        // Top sites
+        mTopSitesAdapter = new TopSitesAdapterImpl(mTopSitesClickListener);
+        mBinding.topSitesList.setAdapter(mTopSitesAdapter);
+        mBinding.topSitesList.setHasFixedSize(true);
+
+        TopSitesHelper topSitesHelper = new TopSitesHelper(getContext(), ((VRBrowserActivity) getContext()).getCoroutineScope());
+        mTopSitesFeature = topSitesHelper.createFeature(mTopSitesAdapter);
+        mTopSitesFeature.start();
     }
+
+    private final TopSitesAdapter.ClickListener mTopSitesClickListener =
+            new TopSitesAdapter.ClickListener() {
+                @Override
+                public void onClicked(@NonNull TopSite site) {
+                    openUrl(site.getUrl());
+                }
+
+                @Override
+                public void onRemoved(@NonNull TopSite site) {
+                    // TODO
+                }
+
+                @Override
+                public void onPinned(@NonNull TopSite site) {
+                    // TODO
+                }
+            };
 
     private void openUrl(@NonNull String url) {
         Session session = SessionStore.get().getActiveSession();
         session.loadUri(url);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mTopSitesFeature.start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mTopSitesFeature.stop();
     }
 }
