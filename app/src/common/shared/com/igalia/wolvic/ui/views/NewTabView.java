77023daf991b90ed2c728fd736c84a0ae7ee8c9b
Felipe Erias <felipeerias@igalia.com>
2025-03-05 13:42:13 +0900

    Logo in New Tab
    Add a Wolvic logo to the New Tab view.
    
    When clicked, it opens wolvic.com

--- app/src/common/shared/com/igalia/wolvic/ui/views/NewTabView.java
+++ app/src/common/shared/com/igalia/wolvic/ui/views/NewTabView.java
@@ -5,18 +5,24 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.VRBrowserActivity;
+import com.igalia.wolvic.browser.engine.Session;
+import com.igalia.wolvic.browser.engine.SessionStore;
 import com.igalia.wolvic.databinding.NewTabBinding;
-import com.igalia.wolvic.ui.widgets.WidgetManagerDelegate;
+import com.igalia.wolvic.ui.viewmodel.SettingsViewModel;
+import com.igalia.wolvic.utils.SystemUtils;
 
 public class NewTabView extends FrameLayout {
 
-    private WidgetManagerDelegate mWidgetManager;
+    static final String LOGTAG = SystemUtils.createLogtag(NewTabView.class);
 
     private NewTabBinding mBinding;
+    private SettingsViewModel mSettingsViewModel;
 
     public NewTabView(Context context) {
         super(context);
@@ -24,7 +30,6 @@ public class NewTabView extends FrameLayout {
     }
 
     protected void initialize() {
-        mWidgetManager = ((VRBrowserActivity) getContext());
         updateUI();
     }
 
@@ -34,9 +39,17 @@ public class NewTabView extends FrameLayout {
 
         LayoutInflater inflater = LayoutInflater.from(getContext());
 
+        mSettingsViewModel = new ViewModelProvider((VRBrowserActivity) getContext(), ViewModelProvider.AndroidViewModelFactory.getInstance(((VRBrowserActivity) getContext()).getApplication())).get(SettingsViewModel.class);
+
         mBinding = DataBindingUtil.inflate(inflater, R.layout.new_tab, this, true);
         mBinding.setLifecycleOwner((VRBrowserActivity) getContext());
+        mBinding.setSettingsmodel(mSettingsViewModel);
 
-        mBinding.executePendingBindings();
+        mBinding.logo.setOnClickListener(v -> openUrl(getContext().getString(R.string.home_page_url)));
+    }
+
+    private void openUrl(@NonNull String url) {
+        Session session = SessionStore.get().getActiveSession();
+        session.loadUri(url);
     }
 }
