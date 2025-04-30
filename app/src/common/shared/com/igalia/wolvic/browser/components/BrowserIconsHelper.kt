1f2d9ef63c24a4e3e91ab0e518f2e2f94b497219
Felipe Erias <felipeerias@igalia.com>
2025-04-30 12:31:17 +0900

    Load the Wolvic.com favicon for local pages
    When loading a local about:// page, the corresponding
    tab will show the Wolvic launcher icon.

--- app/src/common/shared/com/igalia/wolvic/browser/components/BrowserIconsHelper.kt
+++ app/src/common/shared/com/igalia/wolvic/browser/components/BrowserIconsHelper.kt
@@ -2,7 +2,9 @@ package com.igalia.wolvic.browser.components
 
 import android.content.Context
 import android.widget.ImageView
+import com.igalia.wolvic.R
 import com.igalia.wolvic.browser.engine.EngineProvider
+import com.igalia.wolvic.utils.UrlUtils
 import mozilla.components.browser.icons.BrowserIcons
 import mozilla.components.browser.icons.IconRequest
 import mozilla.components.browser.state.store.BrowserStore
@@ -11,7 +13,8 @@ import mozilla.components.concept.engine.Engine
 // Small helper class to simplify getting favicons.
 class BrowserIconsHelper(context: Context, engine: Engine, store: BrowserStore) {
 
-    private val browserIcons: BrowserIcons;
+    private val browserIcons: BrowserIcons
+    private val aboutPageFavicon = R.mipmap.ic_launcher
 
     init {
         browserIcons =
@@ -24,8 +27,12 @@ class BrowserIconsHelper(context: Context, engine: Engine, store: BrowserStore) 
         url: String,
         size: IconRequest.Size = IconRequest.Size.DEFAULT
     ) {
-        val request = IconRequest(url, size, emptyList(), null, false)
-        browserIcons.loadIntoView(view, request, null, null)
+        if (UrlUtils.isAboutPage(url)) {
+            view.setImageResource(aboutPageFavicon)
+        } else {
+            val request = IconRequest(url, size, emptyList(), null, false)
+            browserIcons.loadIntoView(view, request, null, null)
+        }
     }
 
     fun loadIntoView(
