217c332e98d89a66a8db8ebf9f406b9468bd1175
Felipe Erias <felipeerias@igalia.com>
2026-09-23 00:47:31 +0900

    Fix accidentally ignoring user search engine preference
    There is a race condition in setupSearchEngine() because the list of search engines is loaded asynchronously and we validate the user preference against it. So if the list is still empty, the user preference is ignored and currentSearchEngine remains null. Afterwards, updateSearchEngine() uses that null value instead of the saved preference and selects the default engine.

--- app/src/common/shared/com/igalia/wolvic/search/SearchEngineWrapper.kt
+++ app/src/common/shared/com/igalia/wolvic/search/SearchEngineWrapper.kt
@@ -113,7 +113,7 @@ class SearchEngineWrapper private constructor(aContext: Context) :
 
     val availableSearchEngines: Collection<SearchEngine>
         get() {
-            updateSearchEngine()
+            setupPreferredSearchEngine()
             val allEngines = mutableListOf<SearchEngine>()
             mSearchEnginesMap?.values?.let { allEngines.addAll(it) }
             val customEngines = getCustomSearchEngines()
@@ -268,16 +268,6 @@ class SearchEngineWrapper private constructor(aContext: Context) :
 
     private fun hasContext(): Boolean {
         return mContextRef.get() != null
-    }
-
-    private fun updateSearchEngine() {
-        val newSearchEngineName = if (currentSearchEngine != null) {
-            currentSearchEngine!!.id
-        } else {
-            null
-        }
-
-        setupSearchEngine(null, newSearchEngineName)
     }
 
     private val context: Context?
